95c9c92cdfa0ab7f07e164fbdb8b95e1f49acbb6
WeijieChen2017 <weijiechen1994@gmail.com>
2024-06-18 16:08:10 -0500

    Good morning!

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,4 @@
-from .encoder import Encoder_vqvae_conv
-from .decoder import Decoder_vqvae_conv
+# from .encoder_conv import Encoder_vqvae_conv
+# from .decoder_conv import Decoder_vqvae_conv
+
+from .vitvqgan import ViTVQ3D
