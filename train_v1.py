286ea140751933619344956e1a4fe1786db6ecce
WeijieChen2017 <weijiechen1994@gmail.com>
2024-06-18 23:42:36 -0500

    Good morning!

--- train_v1.py
+++ train_v1.py
@@ -1,13 +1,62 @@
-# load ViTVQ3D from modules
+# ------------------------------------------------------------------------------------
+# Enhancing Transformers
+# Copyright (c) 2022 Thuan H. Nguyen. All Rights Reserved.
+# Licensed under the MIT License [see LICENSE for details]
+# ------------------------------------------------------------------------------------
 
-from modules import ViTVQ3D
+import os
+import sys
+import argparse
+from pathlib import Path
+from omegaconf import OmegaConf
+import pytorch_lightning as pl
 
-# def __init__(self, volume_key: str, volume_size: int, patch_size: int, encoder: OmegaConf, decoder: OmegaConf, quantizer: OmegaConf,
-#                  loss: OmegaConf, path: Optional[str] = None, ignore_keys: List[str] = list(), scheduler: Optional[OmegaConf] = None) -> None:
+from enhancing.utils.general import get_config_from_file, initialize_from_config, setup_callbacks
 
-model = ViTVQ3D(volume_key="volume", volume_size=64, patch_size=8, encoder={"dim": 256, "depth": 4, "heads": 4, "mlp_dim": 512, "dropout": 0.1},
-                decoder={"dim": 256, "depth": 4, "heads": 4, "mlp_dim": 512, "dropout": 0.1},
-                quantizer={"embed_dim": 64, "num_embeddings": 512, "commitment_cost": 0.25, "decay": 0.99},
-                loss={"recon_loss": {"type": "mse", "params": {}}, "commitment_loss": {"type": "vq", "params": {}}},
-                path=None, ignore_keys=list(), scheduler=None)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str, required=True)
+    parser.add_argument('-s', '--seed', type=int, default=0)
+    parser.add_argument('-nn', '--num_nodes', type=int, default=1)
+    parser.add_argument('-ng', '--num_gpus', type=int, default=1)
+    parser.add_argument('-u', '--update_every', type=int, default=1)
+    parser.add_argument('-e', '--epochs', type=int, default=100)
+    parser.add_argument('-lr', '--base_lr', type=float, default=4.5e-6)
+    parser.add_argument('-a', '--use_amp', default=False, action='store_true')
+    parser.add_argument('-b', '--batch_frequency', type=int, default=750)
+    parser.add_argument('-m', '--max_images', type=int, default=4)
+    args = parser.parse_args()
 
+    # Set random seed
+    pl.seed_everything(args.seed)
+
+    # Load configuration
+    config = get_config_from_file(Path("configs")/(args.config+".yaml"))
+    print(config)
+    exp_config = OmegaConf.create({"name": args.config, "epochs": args.epochs, "update_every": args.update_every,
+                                   "base_lr": args.base_lr, "use_amp": args.use_amp, "batch_frequency": args.batch_frequency,
+                                   "max_images": args.max_images})
+
+    # Build model
+    model = initialize_from_config(config.model)
+    model.learning_rate = exp_config.base_lr
+
+    # Setup callbacks
+    callbacks, logger = setup_callbacks(exp_config, config)
+
+    # Build data modules
+    data = initialize_from_config(config.dataset)
+    data.prepare_data()
+
+    # Build trainer
+    trainer = pl.Trainer(max_epochs=exp_config.epochs,
+                         precision=16 if exp_config.use_amp else 32,
+                         callbacks=callbacks,
+                         gpus=args.num_gpus,
+                         num_nodes=args.num_nodes,
+                         strategy="ddp" if args.num_nodes > 1 or args.num_gpus > 1 else None,
+                         accumulate_grad_batches=exp_config.update_every,
+                         logger=logger)
+
+    # Train
+    trainer.fit(model, data)
