797553ecda480774d1458871ae2c5d505c9c0fab
WeijieChen2017 <weijiechen1994@gmail.com>
2024-06-27 17:30:23 -0500

    Good morning!

--- run_v1.sh
+++ run_v1.sh
@@ -6,6 +6,12 @@ rm tsv1_ct.tar.gz
 mv tsv1_ct ./SharkSeagrass/
 mv model_best_181_state_dict.pth ./SharkSeagrass/
 cd SharkSeagrass
+mkdir cache
+cd cache
+mkdir wandb
+cd ..
+export WANDB_API_KEY="41c33ee621453a8afcc7b208674132e0e8bfafdb"
+export WANDB_DIR="cache/wandb"
 python train_v2_vq.py
 rm -rf tsv1_ct
 cd ../
