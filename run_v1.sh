9fb94d669f0b1724523440e65f61033ebbe18822
WeijieChen2017 <weijiechen1994@gmail.com>
2024-06-26 19:15:27 -0500

    Good morning!

--- run_v1.sh
+++ run_v1.sh
@@ -4,6 +4,7 @@ unzip SharkSeagrass.zip
 rm SharkSeagrass.zip
 rm tsv1_ct.tar.gz
 mv tsv1_ct ./SharkSeagrass/
+mv model_best_181_state_dict.pth ./SharkSeagrass/
 cd SharkSeagrass
 python train_v1.py
 rm -rf tsv1_ct
