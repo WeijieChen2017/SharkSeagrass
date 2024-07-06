427b83e74853a2ac5e02d74794b33b6c79d5fd7d
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-06 00:20:58 -0500

    Good morning!

---
+++ run_v4.sh
@@ -0,0 +1,20 @@
+pip list
+tar -xvzf tsv1_ct.tar.gz
+unzip SharkSeagrass.zip
+rm SharkSeagrass.zip
+rm tsv1_ct.tar.gz
+mv tsv1_ct ./SharkSeagrass/
+mv model_best_181_state_dict.pth ./SharkSeagrass/
+cd SharkSeagrass
+ls
+echo "============================================"
+python train_v4_pyramid.py
+rm -rf tsv1_ct
+find . -name wandb-metadata.json
+mv cache ./results/
+tar -czvf SharkSeagrass_results_$(date +"%m_%d_%H_%M").tar.gz results
+echo "============================================"
+ls
+mv SharkSeagrass_results_$(date +"%m_%d_%H_%M").tar.gz ../
+echo "============================================"
+ls
