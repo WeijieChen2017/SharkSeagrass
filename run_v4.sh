39124f2979194433aa5673f24eb23fa89b67a24e
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-06 01:28:10 -0500

    Good morning!

--- run_v4.sh
+++ run_v4.sh
@@ -1,8 +1,8 @@
 pip list
-tar -xvf tsv1_ct_80.tar.gz
+tar -xvf tsv1_ct_80.tar
 unzip SharkSeagrass.zip
 rm SharkSeagrass.zip
-rm tsv1_ct_80.tar.gz
+rm tsv1_ct_80.tar
 mv tsv1_ct_80 ./SharkSeagrass/
 mv model_best_181_state_dict.pth ./SharkSeagrass/
 cd SharkSeagrass
