0dfd11e1c08eaf7c25b97418ecfa9f03d5a98d74
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-24 17:18:37 -0500

    Good morning!

--- cut_whole_body_data.py
+++ cut_whole_body_data.py
@@ -28,6 +28,46 @@ def calculate_percentiles(data, q_list):
     percentiles = [(np.sum(flatten_data < q) / flatten_data.size) * 100 for q in q_list]
     return percentiles
 
+def save_data_at_z1_to_z2(data_1, data_2, loc_z, thickness_pixel, tag, affine_1, header_1, affine_2, header_2):
+
+    z1 = loc_z
+    z2 = loc_z + thickness_pixel
+
+    data_1_z = data_1[:, :, z1:z2]
+    data_2_z = data_2[:, :, z1:z2]
+
+    print("Cropped data from ", z1, " to ", z2)
+
+    data_1_z_nii = nib.Nifti1Image(data_1_z, affine_1, header_1)
+    data_2_z_nii = nib.Nifti1Image(data_2_z, affine_2, header_2)
+
+    savename_1 = f"synCT_PET_James/crop/{tag}_PET_thick_256_s{z1:03d}e{z2:03d}.nii.gz"
+    savename_2 = f"synCT_PET_James/crop/{tag}_CT_thick_256_s{z1:03d}e{z2:03d}.nii.gz"
+
+    nib.save(data_1_z_nii, savename_1)
+    nib.save(data_2_z_nii, savename_2)
+
+    print(f"---Cropped data saved at {savename_1}")
+    print(f"---Cropped data saved at {savename_2}")
+
+    # PET_data_crop = PET_data_clip[:, :, loc_z:loc_z+thickness_pixel]
+    # CT_data_crop = CT_data_clip[:, :, loc_z:loc_z+thickness_pixel]
+    # print("Cropped PET data from ", loc_z, " to ", loc_z+thickness_pixel)
+    # print("Cropped CT data from ", loc_z, " to ", loc_z+thickness_pixel)
+
+    # PET_data_crop_nii = nib.Nifti1Image(PET_data_crop, PET_file.affine, PET_file.header)
+    # CT_data_crop_nii = nib.Nifti1Image(CT_data_crop, CT_file.affine, CT_file.header)
+
+    # savename_PET = f"synCT_PET_James/crop/{tag}_PET_{loc_z}.nii.gz"
+    # savename_CT = f"synCT_PET_James/crop/{tag}_CT_{loc_z}.nii.gz"
+
+    # nib.save(PET_data_crop_nii, savename_PET)
+    # nib.save(CT_data_crop_nii, savename_CT)
+
+    # print(f"---Cropped PET data saved at {savename_PET}")
+    # print(f"---Cropped CT data saved at {savename_CT}")
+
+
 for tag in tags_list[:2]:
 
     PET_file_path = f"synCT_PET_James/ori/{tag}_PET_re.nii.gz"
@@ -78,30 +118,27 @@ for tag in tags_list[:2]:
     # for example, 0 to 200, then 100 to 300, then 200 to 400
     loc_z = 0
     while loc_z + thickness_pixel <= len_z:
-        PET_data_crop = PET_data_clip[:, :, loc_z:loc_z+thickness_pixel]
-        CT_data_crop = CT_data_clip[:, :, loc_z:loc_z+thickness_pixel]
-        print("Cropped PET data from ", loc_z, " to ", loc_z+thickness_pixel)
-        print("Cropped CT data from ", loc_z, " to ", loc_z+thickness_pixel)
-
-        # PET_data_crop_nii = nib.Nifti1Image(PET_data_crop, PET_file.affine, PET_file.header)
-        # CT_data_crop_nii = nib.Nifti1Image(CT_data_crop, CT_file.affine, CT_file.header)
-
-        # savename_PET = f"synCT_PET_James/crop/{tag}_PET_{loc_z}.nii.gz"
-        # savename_CT = f"synCT_PET_James/crop/{tag}_CT_{loc_z}.nii.gz"
-
-        # nib.save(PET_data_crop_nii, savename_PET)
-        # nib.save(CT_data_crop_nii, savename_CT)
-
-        # print(f"---Cropped PET data saved at {savename_PET}")
-        # print(f"---Cropped CT data saved at {savename_CT}")
+        save_data_at_z1_to_z2(PET_data_clip, 
+                              CT_data_clip, 
+                              loc_z, 
+                              thickness_pixel, 
+                              tag, 
+                              PET_file.affine, 
+                              PET_file.header, 
+                              CT_file.affine, 
+                              CT_file.header)
 
         loc_z += thickness_pixel - overlap
 
     # last one is from len_z - thickness_pixel to len_z
-    PET_data_crop = PET_data_clip[:, :, len_z-thickness_pixel:len_z]
-    CT_data_crop = CT_data_clip[:, :, len_z-thickness_pixel:len_z]
-    print("Cropped PET data from ", len_z-thickness_pixel, " to ", len_z)
-    print("Cropped CT data from ", len_z-thickness_pixel, " to ", len_z)
-
+    save_data_at_z1_to_z2(PET_data_clip, 
+                          CT_data_clip, 
+                          len_z - thickness_pixel, 
+                          thickness_pixel, 
+                          tag, 
+                          PET_file.affine, 
+                          PET_file.header, 
+                          CT_file.affine, 
+                          CT_file.header)
 
     print("="*40)
