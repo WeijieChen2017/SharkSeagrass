91e0875690252d5c0871f5de31bf9ef8b7720d92
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-24 16:14:32 -0500

    Good morning!

--- cut_whole_body_data.py
+++ cut_whole_body_data.py
@@ -35,7 +35,7 @@ for tag in tags_list:
     PET_data = PET_file.get_fdata()
     CT_data = CT_file.get_fdata()
 
-    print(tag)
+    print("["*6, tag, "]"*6)
     print("PET data shape: ", PET_data.shape, "CT data shape: ", CT_data.shape)
     print(f"PET data max: {np.max(PET_data):.4f}, PET data min: {np.min(PET_data):.4f}")
     print(f"CT data max: {np.max(CT_data):.4f}, CT data min: {np.min(CT_data):.4f}")
@@ -43,24 +43,26 @@ for tag in tags_list:
     for q in Q_list:
         p_PET = np.percentile(PET_data, q)
         p_CT = np.percentile(CT_data, q)
-        print(f">{q:.4f}% percentile of PET: {p_PET:.4f}, {q:.4f}% percentile of CT: {p_CT:.4f}")
+        print(f">{q:.2f}% percentile of PET: {p_PET:.4f}, {q:.2f}% percentile of CT: {p_CT:.4f}")
     # output the percentile Q_list_PET and Q_list_CT
     # it is where the value in Q_list_PET is the percentage of the PET_data
     # for example, if Q_list_PET = 3000, then the value is the 75% percentile of PET_data
     for idx_Q in range(len(Q_list_PET)):
         pQ_PET = calculate_percentiles(PET_data, Q_list_PET)[idx_Q]
         pQ_CT = calculate_percentiles(CT_data, Q_list_CT)[idx_Q]
-        print(f"<{pQ_PET:.4f}% of PET: {Q_list_PET[idx_Q]}, {pQ_CT:.4f}% of CT: {Q_list_CT[idx_Q]}")
+        print(f"<{pQ_PET:.2f}% of PET: {Q_list_PET[idx_Q]}, {pQ_CT:.2f}% of CT: {Q_list_CT[idx_Q]}")
 
-    print("="*40)
+    
 
     # # original_CT is 467*467*730
     # # this should be cropped to 400*400*730
     CT_data_crop = CT_data[33:433, 33:433, :]
-    print(CT_data_crop.shape)
+    print("New CT data shape: ", CT_data_crop.shape)
 
     # # save the cropped CT data
     CT_data_crop_nii = nib.Nifti1Image(CT_data_crop, PET_file.affine, PET_file.header)
     savename = f"synCT_PET_James/ori/{tag}_CT_400.nii.gz"
     nib.save(CT_data_crop_nii, savename)
-    print("Cropped CT data saved at ", savename)
+    print("---Cropped CT data saved at ", savename)
+
+    print("="*40)
