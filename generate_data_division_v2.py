2cb4bdd4b3b64e87ddc8e1013587460ae09673b6
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-24 22:56:08 -0500

    Good morning!

--- generate_data_division_v2.py
+++ generate_data_division_v2.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-tags_list = sorted(glob.glob("synCT_PET_James/ori/E*.nii.gz"))
+tags_list = sorted(glob.glob("synCT_PET_James/E*.nii.gz"))
 tags_list = [os.path.basename(tag)[:5] for tag in tags_list]
 print(tags_list)
 print("="*40)
@@ -13,6 +13,7 @@ print(tags_list)
 print("="*40)
 
 # compute how many files for each tag
-n_files = {tag: len(glob.glob(f"synCT_PET_James/ori/{tag}*")) for tag in tags_list}
+n_files = {tag: len(glob.glob(f"synCT_PET_James/{tag}*")) for tag in tags_list}
 for tag, n in n_files.items():
     print(f"{tag}: {n} files")
+
