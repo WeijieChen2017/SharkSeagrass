03eec29a99e1e7466921880f2f4cc6b21cf8d8d2
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-24 23:01:56 -0500

    Good morning!

--- generate_data_division_v2.py
+++ generate_data_division_v2.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import random
 
 tags_list = sorted(glob.glob("synCT_PET_James/E*.nii.gz"))
 tags_list = [os.path.basename(tag)[:5] for tag in tags_list]
@@ -39,24 +40,22 @@ for tag, n in n_files.items():
 
 # Divide the files into 5 chunks, where each chunk contains the similar number of files
 n_chunks = 5
-n_files_per_chunk = sum(n_files.values()) // n_chunks
-print(f"Each chunk will contain {n_files_per_chunk} files")
+n_try = 100
+best_equalness = 100
+chunk_division = []
 
-# Divide the files into chunks
-chunks = []
-chunk = []
-chunk_size = 0
-for tag, n in n_files.items():
-    if chunk_size + n <= n_files_per_chunk:
-        chunk.append(tag)
-        chunk_size += n
-    else:
-        chunks.append(chunk)
-        chunk = [tag]
-        chunk_size = n
-chunks.append(chunk)
-
-# Print the chunks
-for i, chunk in enumerate(chunks):
-    print(f"Chunk {i+1}: {chunk}", "number of files:", sum([n_files[tag] for tag in chunk]))
-
+# randomly shuffle the tags and compute the number of files for each tag
+for _ in range(n_try):
+    random.shuffle(tags_list)
+    for i in range(n_chunks):
+        chunk_tags = tags_list[i::n_chunks]
+        chunk_n_files = sum([n_files[tag] for tag in chunk_tags])
+        chunk_division.append((chunk_tags, chunk_n_files))
+    # compute the equalness of the number of files in each chunk
+    equalness = max([n_files for _, n_files in chunk_division]) - min([n_files for _, n_files in chunk_division])
+    if equalness < best_equalness:
+        best_equalness = equalness
+        best_chunk_division = chunk_division
+        print(f"Best equalness: {best_equalness}")
+        print(best_chunk_division)
+    
