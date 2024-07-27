d195925484f0e41e804e98619c38ef6d2d0142b2
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-27 18:43:28 -0500

    Good morning!

--- scan_dataset.py
+++ scan_dataset.py
@@ -3,7 +3,7 @@ import json
 import nibabel as nib
 import numpy as np
 
-json_file = "synCT_PET_James/3PET1CT.json"
+json_file = "synCT_PET_James/3PET1CT_npy.json"
 
 with open(json_file, "r") as f:
     data = json.load(f)
@@ -18,12 +18,14 @@ for chunk_name, sub_tag_dict_list in data.items():
         chunk_npy_dict = {}
         for modality, file_path in sub_tag_dict.items():
             if os.path.exists(file_path):
-                data = nib.load(file_path).get_fdata()
+                data = np.load(file_path, allow_pickle=True).item()
                 print(f"{file_path} file exists and ", f"shape: {data.shape}")
-                npy_file_path = file_path.replace(".nii.gz", ".npy")
-                np.save(npy_file_path, data)
-                print(f"---Numpy file saved at {npy_file_path}")
-                chunk_npy_dict[modality] = npy_file_path
+                # data = nib.load(file_path).get_fdata()
+                # print(f"{file_path} file exists and ", f"shape: {data.shape}")
+                # npy_file_path = file_path.replace(".nii.gz", ".npy")
+                # np.save(npy_file_path, data)
+                # print(f"---Numpy file saved at {npy_file_path}")
+                # chunk_npy_dict[modality] = npy_file_path
             else:
                 print(f"{modality} file does not exist.")
                 print(file_path)
@@ -33,7 +35,7 @@ for chunk_name, sub_tag_dict_list in data.items():
     print("="*40)
 
 # save the npy_dict as a json file
-npy_json_file = json_file.replace(".json", "_npy.json")
-with open(npy_json_file, "w") as json_file:
-    json.dump(npy_dict, json_file, indent=4)
-    print(f"---Numpy files saved at {npy_json_file}")
+# npy_json_file = json_file.replace(".json", "_npy.json")
+# with open(npy_json_file, "w") as json_file:
+#     json.dump(npy_dict, json_file, indent=4)
+#     print(f"---Numpy files saved at {npy_json_file}")
