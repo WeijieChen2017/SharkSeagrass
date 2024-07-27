6710ca8e2c24a6de0d55275e7d6e2a371b29de71
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-27 15:42:34 -0500

    Good morning!

--- basic_UNet_scan_dataset.py
+++ basic_UNet_scan_dataset.py
@@ -9,4 +9,7 @@ for chunk_name, sub_tag_dict_list in dataset.items():
     print(f"---{chunk_name}---")
     for sub_tag_dict in sub_tag_dict_list:
         print(sub_tag_dict)
+        for image_modality, image_path in sub_tag_dict.items():
+            print(f"---{image_modality}---")
+            print(image_path)
     print("="*40)
