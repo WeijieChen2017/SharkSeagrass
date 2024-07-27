e867a74f0e2cd087d0243e7bddfc88078e322bc9
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-27 17:32:57 -0500

    Good morning!

--- scan_dataset.py
+++ scan_dataset.py
@@ -1,5 +1,6 @@
 import os
 import json
+import nibabel as nib
 
 json_file = "synCT_PET_James/3PET1CT.json"
 
@@ -11,7 +12,8 @@ for chunk_name, sub_tag_dict_list in data.items():
     for sub_tag_dict in sub_tag_dict_list:
         for modality, file_path in sub_tag_dict.items():
             if os.path.exists(file_path):
-                print(f"{modality} file exists.")
+                data = nib.load(file_path).get_fdata()
+                print(f"{modality} file exists and ", f"shape: {data.shape}")
             else:
                 print(f"{modality} file does not exist.")
                 print(file_path)
