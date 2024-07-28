e386133429ca89e3b01970964462358fd9e8dec5
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-28 01:49:25 -0500

    Good morning!

--- deal_with_special_case.py
+++ deal_with_special_case.py
@@ -15,11 +15,25 @@ print("---CT data---")
 print(CT_data.shape)
 cut_data_CT = CT_data[:, :, 1013:1269]
 print(cut_data_CT.shape)
+# clip the data from 
+
 
 print("---PET data---")
 print(PET_data.shape)
 cut_data_PET = PET_data[:, :, 1013:1269]
 print(cut_data_PET.shape)
+
+# PET_data_clip = np.clip(PET_data, 0, 4000)
+# CT_data_clip = np.clip(CT_data, -1024, 2976)
+
+# PET_data_clip = PET_data_clip / 4000
+# CT_data_clip = (CT_data_clip + 1024) / 4000
+
+cut_data_CT = np.clip(cut_data_CT, -1024, 2976)
+cut_data_CT = (cut_data_CT + 1024) / 4000
+
+cut_data_PET = np.clip(cut_data_PET, 0, 4000)
+cut_data_PET = cut_data_PET / 4000
 
 cut_CT_nii = nib.Nifti1Image(cut_data_CT, PET_file.affine, PET_file.header)
 new_CT_nii = "synCT_PET_James/E4063_CT_thick_256_norm01_s1013e1269.nii.gz"
