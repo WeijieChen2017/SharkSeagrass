786c82bc5b56f4103c2d58ad247ae2f637bd4d4b
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-27 22:15:01 -0500

    Good morning!

--- deal_with_special_case.py
+++ deal_with_special_case.py
@@ -3,33 +3,30 @@ import os
 import numpy as np
 
 CT_path = "synCT_PET_James/ori/E4063_CT_400.nii.gz"
+PET_path = "synCT_PET_James/ori/E4063_PET_re.nii.gz"
 
 CT_file = nib.load(CT_path)
 CT_data = CT_file.get_fdata()
 
+PET_file = nib.load(PET_path)
+PET_data = PET_file.get_fdata() 
+
 print("---CT data---")
 print(CT_data.shape)
-cut_data = CT_data[:, :, 1024:1280]
+cut_data_CT = CT_data[:, :, 1013:1269]
+print(cut_data_CT.shape)
 
-print(cut_data.shape)
+print("---PET data---")
+print(PET_data.shape)
+cut_data_PET = PET_data[:, :, 1013:1269]
+print(cut_data_PET.shape)
 
-cut_data_nii = nib.Nifti1Image(cut_data, CT_file.affine, CT_file.header)
+cut_CT_nii = nib.Nifti1Image(cut_data_CT, PET_file.affine, PET_file.header)
+new_CT_nii = "synCT_PET_James/E4063_CT_thick_256_norm01_s1013e1269.nii.gz"
+print(f"---Cropped data saved at {new_CT_nii}")
+# nib.save(cut_CT_nii, new_CT_nii)
 
-new_filename_nii = "synCT_PET_James/E4063_CT_thick_256_norm01_s1024e1280.nii.gz"
-# nib.save(cut_data_nii, new_filename_nii)
-print(f"---Cropped data saved at {new_filename_nii}")
-
-new_filename_npy = "synCT_PET_James/E4063_CT_thick_256_norm01_s1024e1280.npy"
-# np.save(new_filename_npy, cut_data)
-print(f"---Numpy file saved at {new_filename_npy}")
-
-# double check by loading two files and output the shape
-CT_file = nib.load(new_filename_nii)
-CT_data = CT_file.get_fdata()
-print(f"---{new_filename_nii}---")
-print(CT_data.shape)
-
-CT_data = np.load(new_filename_npy)
-print(f"---{new_filename_npy}---")
-print(CT_data.shape)
-
+cut_PET_nii = nib.Nifti1Image(cut_data_PET, PET_file.affine, PET_file.header)
+new_PET_nii = "synCT_PET_James/E4063_PET_thick_256_norm01_s1013e1269.nii.gz"
+print(f"---Cropped data saved at {new_PET_nii}")
+# nib.save(cut_PET_nii, new_PET_nii)
