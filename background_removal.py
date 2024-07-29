3f47231892e8972212129e0018462284a32b4df2
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-29 02:06:22 -0500

    Good morning!

--- background_removal.py
+++ background_removal.py
@@ -6,6 +6,8 @@ import numpy as np
 import scipy.ndimage as nd
 from scipy.ndimage import gaussian_filter
 
+th_list = [0.05, 0.04, 0.06]
+
 def generate_mask(data, threshold=0.05):
     mask_data = np.zeros_like(data)
     mask_data[data >= threshold] = 1
@@ -19,7 +21,7 @@ def generate_mask(data, threshold=0.05):
     mask_data = nd.binary_dilation(mask_data, structure=struct)
 
     # Create a structuring element for x-y plane erosion with radius 7
-    radius_erosion = 7
+    radius_erosion = 15
     struct_xy = np.zeros((radius_erosion*2+1, radius_erosion*2+1, 3))  # Initialize a larger array to accommodate the radius
     struct_xy[:, :, 1] = nd.iterate_structure(nd.generate_binary_structure(2, 1), radius_erosion)  # Set the middle z-slice to a 2D structuring element with radius 7
 
@@ -28,26 +30,34 @@ def generate_mask(data, threshold=0.05):
 
     return mask_data
 
+PET_list = sorted(glob.glob("synCT_PET_James/ori/E4079_PET_re.nii.gz"))
+
+for th_value in th_list:
+    for PET_path in PET_list:
+        
+        PET_file = nib.load(PET_path)
+        PET_data = PET_file.get_fdata()
+
+        PET_data = np.clip(PET_data, 0, 4000)
+        PET_data_smooth = PET_data_smooth / 4000
+        PET_data_smooth = gaussian_filter(PET_data, sigma=3)
+        PET_data_smooth_gradient = np.gradient(PET_data_smooth)
+        PET_data_smooth_gradient_magnitude = np.sqrt(PET_data_smooth_gradient[0]**2 + PET_data_smooth_gradient[1]**2 + PET_data_smooth_gradient[2]**2)
+        
+        Ker3_filename = PET_path.replace("PET_re", f"PET_GauKer3")
+        Ker3_nii = nib.Nifti1Image(PET_data_smooth, PET_file.affine, PET_file.header)
+        nib.save(Ker3_nii, Ker3_filename)
+        print(f"---Smoothed data saved at {Ker3_filename}")
+
+        GradMag_filename = PET_path.replace("PET_re", f"PET_GradMag")
+        GradMag_nii = nib.Nifti1Image(PET_data_smooth_gradient_magnitude, PET_file.affine, PET_file.header)
+        nib.save(GradMag_nii, GradMag_filename)
+        print(f"---Gradient magnitude data saved at {GradMag_filename}")
 
 
-PET_file = "synCT_PET_James/ori/E4079_PET_re.nii.gz"
-PET_file = nib.load(PET_file)
-PET_data = PET_file.get_fdata()
+        PET_mask = generate_mask(PET_data_smooth, threshold=th_value)
 
-PET_data = np.clip(PET_data, 0, 4000)
-PET_data_smooth = gaussian_filter(PET_data, sigma=3)
-PET_data_smooth = PET_data_smooth / 4000
-
-Ker3_filename = "synCT_PET_James/ori/E4079_PET_400_GauKer_3.nii.gz"
-Ker3_nii = nib.Nifti1Image(PET_data_smooth, PET_file.affine, PET_file.header)
-nib.save(Ker3_nii, Ker3_filename)
-print(f"---Smoothed data saved at {Ker3_filename}")
-
-PET_mask = generate_mask(PET_data_smooth)
-
-PET_mask_nii = nib.Nifti1Image(PET_mask, PET_file.affine, PET_file.header)
-nib.save(PET_mask_nii, "synCT_PET_James/ori/E4079_PET_re_mask.nii.gz")
-print(f"---Mask saved at synCT_PET_James/ori/E4079_PET_re_mask.nii.gz")
-
-
-
+        PET_mask_nii = nib.Nifti1Image(PET_mask, PET_file.affine, PET_file.header)
+        PET_mask_filename = PET_path.replace("PET_re", f"PET_mask_{th_value*100:02d}")
+        nib.save(PET_mask_nii, PET_mask_filename)
+        print(f"---Mask saved at {PET_mask_filename}")
