5a3f1e5d462daee07a4b1e8841ff116f7433f2ea
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-30 15:07:19 -0500

    Good morning!

--- resize_dataset.py
+++ resize_dataset.py
@@ -1,11 +1,15 @@
 import glob
 import os
 
-file_list = glob.glob("*.nii.gz")
+file_list = ["E4063_CT.nii.gz"]
+
+# file_list = sorted(glob.glob("*.nii.gz"))
+
 for filepath in file_list:
     print(filepath)
     filename = os.path.basename(filepath)
-    new_filename = filename.replace(".nii.gz", "_re.nii.gz")
-    cmd = f"3dresample -dxyz 1.5 1.5 1.5 -rmode Cu -prefix {new_filename} -input {filename}"
+    new_filename = filename.replace(".nii.gz", "_400.nii.gz")
+    cmd_ = f"3dresample -dxyz 1.5 1.5 1.5 -rmode Cu -prefix {new_filename} -input {filename}"
     print(cmd)
     os.system(cmd)
+    cmd_mv
