2bb923e4fb3eed9e1b4390f5ea1e52fe0e73af40
WeijieChen2017 <weijiechen1994@gmail.com>
2024-07-30 14:19:11 -0500

    Good morning!

--- inspect_data.py
+++ inspect_data.py
@@ -12,26 +12,33 @@ tag_list = sorted(tag_list)
 PET_list = [f"synCT_PET_James/ori/{tag}_PET.nii.gz" for tag in tag_list]
 CT_list = [f"synCT_PET_James/ori/{tag}_CT.nii.gz" for tag in tag_list]
 
+# for PET_path, CT_path in zip(PET_list, CT_list):
+#     PET_file = nib.load(PET_path)
+#     PET_data = PET_file.get_fdata()
+
+#     CT_file = nib.load(CT_path)
+#     CT_data = CT_file.get_fdata()
+
+#     print("-"*40)
+#     print(PET_path)
+#     print("PET data shape: ", PET_data.shape)
+#     pix_dim = PET_file.header.get_zooms()
+#     print("Pixel dimension: ", pix_dim)
+#     # show the physical dimension
+#     print("Physical dimension: ", [pix_dim[i]*PET_data.shape[i] for i in range(3)])
+
+#     print(CT_path)
+#     print("CT data shape: ", CT_data.shape)
+#     pix_dim = CT_file.header.get_zooms()
+#     print("Pixel dimension: ", pix_dim)
+#     # show the physical dimension
+#     print("Physical dimension: ", [pix_dim[i]*CT_data.shape[i] for i in range(3)])
+#     print("-"*40)
+
 for PET_path, CT_path in zip(PET_list, CT_list):
-    PET_file = nib.load(PET_path)
-    PET_data = PET_file.get_fdata()
+    cmd_PET = f"cp {PET_path} synCT_PET_James/ori/raw/"
+    cmd_CT = f"cp {CT_path} synCT_PET_James/ori/raw/"
 
-    CT_file = nib.load(CT_path)
-    CT_data = CT_file.get_fdata()
-
-    print("-"*40)
-    print(PET_path)
-    print("PET data shape: ", PET_data.shape)
-    pix_dim = PET_file.header.get_zooms()
-    print("Pixel dimension: ", pix_dim)
-    # show the physical dimension
-    print("Physical dimension: ", [pix_dim[i]*PET_data.shape[i] for i in range(3)])
-
-    print(CT_path)
-    print("CT data shape: ", CT_data.shape)
-    pix_dim = CT_file.header.get_zooms()
-    print("Pixel dimension: ", pix_dim)
-    # show the physical dimension
-    print("Physical dimension: ", [pix_dim[i]*CT_data.shape[i] for i in range(3)])
-    print("-"*40)
-
+    os.system(cmd_PET)
+    os.system(cmd_CT)
+    print(f"---{PET_path} and {CT_path} copied to synCT_PET_James/ori/raw/")
