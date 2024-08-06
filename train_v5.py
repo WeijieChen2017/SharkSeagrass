2837dc7181651172749ddd3676eeb46c41852038
WeijieChen2017 <weijiechen1994@gmail.com>
2024-08-06 02:17:44 -0500

    Good morning!

--- train_v5.py
+++ train_v5.py
@@ -390,13 +390,13 @@ def generate_model_levels(global_config):
             "channels": global_config['pyramid_channels'][:i+1],
             "strides": global_config['pyramid_strides'][-(i+1):],
             "num_res_units": global_config['pyramid_num_res_units'][i],
-        },
+        }
         second_encoder = {
             "spatial_dims": 3, "in_channels": len(global_config['input_modality']),
             "channels": global_config['pyramid_channels'][:i+1],
             "strides": global_config['pyramid_strides'][-(i+1):],
             "num_res_units": global_config['pyramid_num_res_units'][i],
-        },
+        }
         quantizer = {
             "dim": global_config['pyramid_channels'][i]*2, 
             "codebook_size": global_config['pyramid_codebook_size'][i],
@@ -411,6 +411,7 @@ def generate_model_levels(global_config):
             "num_res_units": global_config['pyramid_num_res_units'][i],
             "hwd": global_config['pyramid_mini_resolution'],
         }
+
         model_levels.append({
             "encoder": encoder,
             "second_encoder": second_encoder,
