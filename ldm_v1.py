3893cdf7a1c5dbee1b0d26604e87e1b50e71dba4
WeijieChen2017 <weijiechen1994@gmail.com>
2024-08-18 15:05:34 -0500

    Good morning!

--- ldm_v1.py
+++ ldm_v1.py
@@ -438,7 +438,7 @@ class VQModel(pl.LightningModule):
         self.image_key = image_key
         self.encoder = Encoder(**ddconfig)
         self.decoder = Decoder(**ddconfig)
-        self.loss = instantiate_from_config(lossconfig)
+        # self.loss = instantiate_from_config(lossconfig)
         self.quantize = VectorQuantizer(n_embed, embed_dim, beta=0.25,
                                         remap=remap,
                                         sane_index_shape=sane_index_shape)
