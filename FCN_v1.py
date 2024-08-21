81e05e0bd4abdcd1b4d800e63dd2a534d5221b15
WeijieChen2017 <weijiechen1994@gmail.com>
2024-08-21 00:02:28 -0500

    Good morning!

--- FCN_v1.py
+++ FCN_v1.py
@@ -42,14 +42,14 @@ class FullyConnected(nn.Module):
 
 tag_list = [
     "E4055", "E4058", "E4061",          "E4066",
-    "E4068", "E4069", "E4073", "E4074", "E4077",
-    "E4078", "E4079",          "E4081", "E4084",
-             "E4091", "E4092", "E4094", "E4096",
-             "E4098", "E4099",          "E4103",
-    "E4105", "E4106", "E4114", "E4115", "E4118",
-    "E4120", "E4124", "E4125", "E4128", "E4129",
-    "E4130", "E4131", "E4134", "E4137", "E4138",
-    "E4139",
+    # "E4068", "E4069", "E4073", "E4074", "E4077",
+    # "E4078", "E4079",          "E4081", "E4084",
+    #          "E4091", "E4092", "E4094", "E4096",
+    #          "E4098", "E4099",          "E4103",
+    # "E4105", "E4106", "E4114", "E4115", "E4118",
+    # "E4120", "E4124", "E4125", "E4128", "E4129",
+    # "E4130", "E4131", "E4134", "E4137", "E4138",
+    # "E4139",
 ]
 
 import os
@@ -166,7 +166,7 @@ for epoch in range(num_epoch):
     for train_dict in train_dataset:
         input_full = train_dict["PET_data"]
         output_full = train_dict["CTr_data"]
-        # print("Input shape:", input_full.shape, output_full.shape)
+        print("Input shape:", input_full.shape, output_full.shape)
         # (730, 400, 400) (730, 400, 400)
         
         for i in range(0, len(input_full), batch_size):
