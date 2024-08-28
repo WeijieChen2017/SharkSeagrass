1f233c8c86223b01e8118ca2e1823e31337c6363
WeijieChen2017 <weijiechen1994@gmail.com>
2024-08-28 02:13:07 -0500

    Good morning!

---
+++ ldm_unet_v1_bedremove.py
@@ -0,0 +1,19 @@
+import numpy as np
+import nibabel as nib
+import os
+import glob
+import json
+
+save_folder = "./B100/f4noattn_step1/"
+
+target_tags = ["E4061", "E4098", "E4114", "E4128", "E4139"]
+
+for tag in target_tags:
+    nii_path = "./B100/nifti/CTACIVV_"+tag+".nii.gz"
+    nii_file = nib.load(nii_path)
+    nii_data = nii_file.get_fdata()
+    print("Processing", nii_path)
+    print("Data shape:", nii_data.shape)
+    for idz in range(nii_data.shape[2]):
+        img = nii_data[:, :, idz]
+        print("Slice", idz, "max:", np.max(img), "min:", np.min(img), img.shape)
