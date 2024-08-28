ef93f4fe3e63b95a7b5425085f23a0d96994b134
WeijieChen2017 <weijiechen1994@gmail.com>
2024-08-28 01:46:11 -0500

    Good morning!

---
+++ ldm_unet_v1_rename.py
@@ -0,0 +1,18 @@
+import numpy as np
+import os
+import glob
+import json
+
+save_folder = "./B100/f4noattn_step1/"
+json_path = "./B100/f4noattn_step1_0822_2d3c.json"
+
+os.makedirs(save_folder, exist_ok=True)
+# load the json file
+with open(json_path, 'r') as file:
+    data_division = json.load(file)
+
+train_list = data_division['train']
+val_list = data_division['val']
+test_list = data_division['test']
+
+print("Train:", len(train_list), "Val:", len(val_list), "Test:", len(test_list))
