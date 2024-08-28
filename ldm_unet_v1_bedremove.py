9520aaa77ab72f654f634f3a11e3b7ee7f455233
WeijieChen2017 <weijiechen1994@gmail.com>
2024-08-28 02:15:51 -0500

    Good morning!

--- ldm_unet_v1_bedremove.py
+++ ldm_unet_v1_bedremove.py
@@ -14,6 +14,6 @@ for tag in target_tags:
     nii_data = nii_file.get_fdata()
     for idz in range(nii_data.shape[2]):
         img = nii_data[:, :, idz]
-        npy_path = save_folder + "STEP2_"+tag+"_"+str(idz)+".npy"
+        npy_path = save_folder + "STEP2_"+tag+"_"+str(idz+1)+".npy"
         np.load(npy_path)
         print(f"Processing {nii_path}. the img shape is {img.shape} and the npy shape is {npy.shape}")
