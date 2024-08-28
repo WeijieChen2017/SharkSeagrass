0120c11f69e8862d9f9839fbb8cc0a727a130fe3
WeijieChen2017 <weijiechen1994@gmail.com>
2024-08-28 01:53:01 -0500

    Good morning!

--- ldm_unet_v1_rename.py
+++ ldm_unet_v1_rename.py
@@ -21,7 +21,7 @@ new_train_list = []
 new_val_list = []
 new_test_list = []
 
-for pair in train_list[:10]:
+for pair in train_list:
     step_1_path = pair["STEP1"]
     step_2_path = pair["STEP2"]
     print("Processing", step_2_path)
@@ -32,5 +32,47 @@ for pair in train_list[:10]:
         "STEP1": step_1_path,
         "STEP2": new_step_2_path,
     }
-    print(rename_cmd)
-    print(new_pair)
+    # print(rename_cmd)
+    # print(new_pair)
+    os.system(rename_cmd)
+    new_train_list.append(new_pair)
+
+for pair in val_list:
+    step_1_path = pair["STEP1"]
+    step_2_path = pair["STEP2"]
+    print("Processing", step_2_path)
+
+    new_step_2_path = step_2_path.replace("PET_TOFNAC", "STEP2")
+    rename_cmd = "mv "+step_2_path+" "+new_step_2_path
+    new_pair = {
+        "STEP1": step_1_path,
+        "STEP2": new_step_2_path,
+    }
+    os.system(rename_cmd)
+    new_val_list.append(new_pair)
+
+for pair in test_list:
+    step_1_path = pair["STEP1"]
+    step_2_path = pair["STEP2"]
+    print("Processing", step_2_path)
+
+    new_step_2_path = step_2_path.replace("PET_TOFNAC", "STEP2")
+    rename_cmd = "mv "+step_2_path+" "+new_step_2_path
+    new_pair = {
+        "STEP1": step_1_path,
+        "STEP2": new_step_2_path,
+    }
+    os.system(rename_cmd)
+    new_test_list.append(new_pair)
+
+new_data_division = {
+    "train": new_train_list,
+    "val": new_val_list,
+    "test": new_test_list,
+}
+
+new_json_path = "./B100/f4noattn_step1_0822_2d3c_rename.json"
+with open(new_json_path, 'w') as file:
+    json.dump(new_data_division, file, indent=4)
+
+print("Done")
