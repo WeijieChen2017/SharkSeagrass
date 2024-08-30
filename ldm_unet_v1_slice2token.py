1ae2f36e08793c251b47ec8af57c0787f8a07531
WeijieChen2017 <weijiechen1994@gmail.com>
2024-08-30 16:53:23 -0500

    Good morning!

--- ldm_unet_v1_slice2token.py
+++ ldm_unet_v1_slice2token.py
@@ -6,6 +6,7 @@ RECON_SLICE_FOLDER = "./B100/f4noattn_step1_recon/"
 TOKEN_SLICE_FOLDER = "./B100/f4noattn_step1_token/"
 VQ_NAME = "f4-noattn"
 
+import os
 for folder in [RECON_SLICE_FOLDER, TOKEN_SLICE_FOLDER]:
     os.makedirs(folder, exist_ok=True)
 
