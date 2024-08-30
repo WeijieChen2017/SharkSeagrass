66e2f9ae92bb8fb21fc98459390f7769fbd5ea69
WeijieChen2017 <weijiechen1994@gmail.com>
2024-08-30 14:51:00 -0500

    Good morning!

--- ldm_unet_v1_slice2volume.py
+++ ldm_unet_v1_slice2volume.py
@@ -1,5 +1,6 @@
 SLICE_FOLDER = "./B100/f4noattn_step1/"
 TOFNAC_FOLDER = "./B100/TOFNAC_resample/"
+STEP1_VOLUME_FOLDER = "./B100/f4noattn_step1_volume/"
 
 MID_PET = 5000
 MIQ_PET = 0.9
@@ -50,15 +51,26 @@ TOFNAC_list = glob.glob(TOFNAC_FOLDER+"*.nii.gz")
 print("Found", len(TOFNAC_list), "TOFNAC files")
 
 for idx, TOFNAC_path in enumerate(TOFNAC_list[:2]):
-    print(f"Processing [{idx+1}]/[{len(TOFNAC_list)}] {TOFNAC_path}")
+    TOFNAC_tag = TOFNAC_path.split('/')[-1].split('.')[0][-5:]
+    print(f"Processing [{idx+1}]/[{len(TOFNAC_list)}] {TOFNAC_path} TOFNAC tag is {TOFNAC_tag}")
     TOFNAC_file = nib.load(TOFNAC_path)
     TOFNAC_data = TOFNAC_file.get_fdata()
     len_z = TOFNAC_data.shape[2]
-    TOFNAC_tag = TOFNAC_path.split('/')[-1].split('.')[0][-5:]
-    print(">>> TOFNAC tag:", TOFNAC_tag)
-
+    synCT_data = np.zeros_like(TOFNAC_data)
+    
     for idz in range(len_z):
         synCT_path = os.path.join(SLICE_FOLDER, f"STEP2_{TOFNAC_tag}_z{idz}.npy")
         # check if the file exists
         if os.path.exists(synCT_path):
-            print("File exists:", synCT_path)
+            synCT_slice = np.load(synCT_path)
+            synCT_slice = np.clip(synCT_slice, 0, 1)
+            synCT_slice = synCT_slice * RANGE_CT + MIN_CT
+            synCT_data[:, :, idz] = synCT_slice
+        else:
+            print(">>> File not found:", synCT_path)
+        
+    # save the synthetic CT
+    synCT_file = nib.Nifti1Image(synCT_data, affine=TOFNAC_file.affine, header=TOFNAC_file.header)
+    synCT_path = os.path.join(STEP1_VOLUME_FOLDER, f"STEP2_{TOFNAC_tag}.nii.gz")
+    nib.save(synCT_file, synCT_path)
+    print(">>> Saved to", synCT_path)
