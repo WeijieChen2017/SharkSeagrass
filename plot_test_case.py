64688874cc6807da3086c554fb1334ab1afa2a29
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-06 12:54:18 -0500

    Good morning!

--- plot_test_case.py
+++ plot_test_case.py
@@ -123,7 +123,10 @@ def plot_case_from_view_cut(x_data, y_data, z_data, save_name, num_cut, cut_view
     
     return mae
 
-
+def out_to_file(file, string):
+    out_to_file(string)
+    with open(file, "a") as f:
+        f.write(string + "\n")
 
 
 data_div_json = "./B100/step1step2_0822_vanila.json"
@@ -133,6 +136,7 @@ with open(data_div_json, "r") as f:
 save_folder = f"./B100/plot_{data_div_to_plot}_case_UNetUNet/"
 if not os.path.exists(save_folder):
     os.makedirs(save_folder)
+log_file = f"{save_folder}log.txt"
 
 train_list = data_div["train"]
 val_list = data_div["val"]
@@ -142,9 +146,9 @@ num_train = len(train_list)
 num_val = len(val_list)
 num_test = len(test_list)
 
-print(f"num_train: {num_train}")
-print(f"num_val: {num_val}")
-print(f"num_test: {num_test}")
+out_to_file(f"num_train: {num_train}")
+out_to_file(f"num_val: {num_val}")
+out_to_file(f"num_test: {num_test}")
 
 axial_cut = 8
 sagittal_cut = 4
@@ -166,41 +170,41 @@ elif data_div_to_plot == "test":
     target_list = test_list
 
 for test_pair in target_list:
-    print()
+    out_to_file()
     x_path = test_pair["STEP1"] # "STEP1": "./B100/f4noattn_step1_volume/STEP1_E4078.nii.gz",
     x_path = x_path.replace("f4noattn_step1_volume_vanila", "TOFNAC_resample")
     x_path = x_path.replace("STEP1", "PET_TOFNAC")
     y_path = test_pair["STEP2"] # "STEP2": "./B100/f4noattn_step2_volume/STEP2_E4078.nii.gz",
     z_path = test_pair["STEP1"].replace("STEP1", "STEP3_d3f64")
     case_name = y_path[-12:-7]
-    print(f"Processing test case: {case_name}")
-    print(f">>> TOFNAC_path: {x_path}")
-    print(f">>> CTAC_path: {y_path}")
-    print(f">>> PRED_path: {z_path}")
+    out_to_file(f"Processing test case: {case_name}")
+    out_to_file(f">>> TOFNAC_path: {x_path}")
+    out_to_file(f">>> CTAC_path: {y_path}")
+    out_to_file(f">>> PRED_path: {z_path}")
 
     x_data = nib.load(x_path).get_fdata()
     y_data = nib.load(y_path).get_fdata()
     z_data = nib.load(z_path).get_fdata()
 
-    print(f">>> TOFNAC_shape: {x_data.shape}, CTAC_shape: {y_data.shape}, PRED_shape: {z_data.shape}")
+    out_to_file(f">>> TOFNAC_shape: {x_data.shape}, CTAC_shape: {y_data.shape}, PRED_shape: {z_data.shape}")
 
 
     # for axial:
     save_name = f"{save_folder}{case_name}_axial_cut_{axial_cut}.png"
     mae = plot_case_from_view_cut(x_data, y_data, z_data, save_name, axial_cut, "axial", None)
-    print(f">>> Saving to {save_name}")
+    out_to_file(f">>> Saving to {save_name}")
 
     # for sagittal:
     save_name = f"{save_folder}{case_name}_sagittal_cut_{sagittal_cut}.png"
     _ = plot_case_from_view_cut(x_data, y_data, z_data, save_name, sagittal_cut, "sagittal", None)
-    print(f">>> Saving to {save_name}")
+    out_to_file(f">>> Saving to {save_name}")
 
     # for coronal:
     save_name = f"{save_folder}{case_name}_coronal_cut_{coronal_cut}.png"
     _ = plot_case_from_view_cut(x_data, y_data, z_data, save_name, coronal_cut, "coronal", None)
-    print(f">>> Saving to {save_name}")
+    out_to_file(f">>> Saving to {save_name}")
 
     mae_list.append(mae)
 
-print()
-print(f"The average MAE of the test cases is {np.mean(mae_list):.2f} HU")
+out_to_file()
+out_to_file(f"The average MAE of the test cases is {np.mean(mae_list):.2f} HU")
