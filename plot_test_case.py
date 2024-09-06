3ed99fbd3c58231b6280934e0e5c62ff03068573
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-06 11:39:22 -0500

    Good morning!

--- plot_test_case.py
+++ plot_test_case.py
@@ -24,8 +24,13 @@ print(f"num_test: {num_test}")
 # plot the test case
 for test_pair in test_list:
     print()
-    x_path = test_pair["STEP1"]
-    y_path = test_pair["STEP2"]
+    x_path = test_pair["STEP1"] # "STEP1": "./B100/f4noattn_step1_volume/STEP1_E4078.nii.gz",
+    x_path = x_path.replace("f4noattn_step1_volume", "TOFNAC_resample")
+    x_path = x_path.replace("STEP1", "PET_TOFNAC")
+    y_path = test_pair["STEP2"] # "STEP2": "./B100/f4noattn_step2_volume/STEP2_E4078.nii.gz",
     z_path = test_pair["STEP1"].replace("STEP1", "STEP3_f364")
-    print(f"Processing test case: {z_path}")
+    print(f"Processing test case:")
+    print(f">>> TOFNAC_path: {x_path}")
+    print(f">>> CTAC_path: {y_path}")
+    print(f">>> PRED_path: {z_path}")
     
