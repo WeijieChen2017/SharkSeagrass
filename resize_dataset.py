f2d8a114382b033e673d074e8d0d0c132269076e
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-12 21:10:11 -0500

    Good morning!

--- resize_dataset.py
+++ resize_dataset.py
@@ -7,9 +7,14 @@ file_list = sorted(glob.glob("*.nii.gz"))
 
 for filepath in file_list:
     # print(filepath)
-    filename = os.path.basename(filepath)
-    new_filename = filename.replace(".nii.gz", "_400.nii.gz")
-    cmd = f"3dresample -dxyz 1.5 1.5 1.5 -rmode Cu -prefix {new_filename} -input {filename}"
-    print(cmd)
+    # filename = os.path.basename(filepath)
+    # new_filename = filename.replace(".nii.gz", "_400.nii.gz")
+    # cmd = f"3dresample -dxyz 1.5 1.5 1.5 -rmode Cu -prefix {new_filename} -input {filename}"
+    # print(cmd)
     # os.system(cmd)
     # print(f"---Resampled data saved at {new_filename}")
+
+    filename = os.path.basename(filepath)
+    new_filename = filename.replace(".nii.gz", "_256.nii.gz")
+    cmd = f"3dresample -dxyz 2.344 2.344 2.344 -rmode Cu -prefix {new_filename} -input {filename}"
+    print(cmd)
