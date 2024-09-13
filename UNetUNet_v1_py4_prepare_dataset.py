d0f928959aaa2f8a20ccc79f8fcaee2c74d6b1a7
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-13 17:58:51 -0500

    Good morning!

--- UNetUNet_v1_py4_prepare_dataset.py
+++ UNetUNet_v1_py4_prepare_dataset.py
@@ -29,6 +29,10 @@ def two_segment_scale(arr, MIN, MID, MAX, MIQ):
     
     return scaled_arr
 
+def printlog(log_file, message):
+    with open(log_file, "a") as f:
+        f.write(message + "\n")
+
 def main():
     argparser = argparse.ArgumentParser(description='Prepare dataset for training')
     argparser.add_argument('-c', '--cross_validation', type=int, default=0, help='Index of the cross validation')
@@ -37,74 +41,85 @@ def main():
     data_div_json = "UNetUNet_v1_data_split.json"
     with open(data_div_json, "r") as f:
         data_div = json.load(f)
-        
-    cv = args.cross_validation
-    train_list = data_div[f"cv_{cv}"]["train"]
-    val_list = data_div[f"cv_{cv}"]["val"]
-    test_list = data_div[f"cv_{cv}"]["test"]
+    
+    for cv in [0,1,2,3,4]:
 
-    # num_train = len(train_list)
-    # num_val = len(val_list)
-    # num_test = len(test_list)
+        cv = args.cross_validation
+        train_list = data_div[f"cv_{cv}"]["train"]
+        val_list = data_div[f"cv_{cv}"]["val"]
+        test_list = data_div[f"cv_{cv}"]["test"]
+        log_file = f"UNetUNet_v1_cv{cv}_log.txt"
 
-    str_train_list = ", ".join(train_list)
-    str_val_list = ", ".join(val_list)
-    str_test_list = ", ".join(test_list)
+        # num_train = len(train_list)
+        # num_val = len(val_list)
+        # num_test = len(test_list)
 
-    # construct the data path list
-    train_path_list = []
-    val_path_list = []
-    test_path_list = []
+        str_train_list = ", ".join(train_list)
+        str_val_list = ", ".join(val_list)
+        str_test_list = ", ".join(test_list)
 
-    for hashname in train_list:
-        train_path_list.append({
-            # BPO124_CTAC_pred_cv0.nii.gz
-            "STEP1": f"B100/UNetUnet_best/cv{cv}/train/{hashname}_CTAC_pred_cv{cv}.nii.gz",
-            # BPO124_CTAC.nii.gz
-            "STEP2": f"B100/UNetUnet_best/CTAC/{hashname}_CTAC.nii.gz",
-        })
+        # construct the data path list
+        train_path_list = []
+        val_path_list = []
+        test_path_list = []
 
-    for hashname in val_list:
-        val_path_list.append({
-            "STEP1": f"B100/UNetUnet_best/cv{cv}/val/{hashname}_CTAC_pred_cv{cv}.nii.gz",
-            "STEP2": f"B100/UNetUnet_best/CTAC/{hashname}_CTAC.nii.gz",
-        })
+        for hashname in train_list:
+            train_path_list.append({
+                # BPO124_CTAC_pred_cv0.nii.gz
+                "STEP1": f"B100/UNetUnet_best/cv{cv}/train/{hashname}_CTAC_pred_cv{cv}.nii.gz",
+                # BPO124_CTAC.nii.gz
+                "STEP2": f"B100/UNetUnet_best/CTAC/{hashname}_CTAC.nii.gz",
+            })
 
-    for hashname in test_list:
-        test_path_list.append({
-            "STEP1": f"B100/UNetUnet_best/cv{cv}/test/{hashname}_CTAC_pred_cv{cv}.nii.gz",
-            "STEP2": f"B100/UNetUnet_best/CTAC/{hashname}_CTAC.nii.gz",
-        })
+        for hashname in val_list:
+            val_path_list.append({
+                "STEP1": f"B100/UNetUnet_best/cv{cv}/val/{hashname}_CTAC_pred_cv{cv}.nii.gz",
+                "STEP2": f"B100/UNetUnet_best/CTAC/{hashname}_CTAC.nii.gz",
+            })
+
+        for hashname in test_list:
+            test_path_list.append({
+                "STEP1": f"B100/UNetUnet_best/cv{cv}/test/{hashname}_CTAC_pred_cv{cv}.nii.gz",
+                "STEP2": f"B100/UNetUnet_best/CTAC/{hashname}_CTAC.nii.gz",
+            })
 
 
-    # train
-    for pair in train_path_list:
-        print("<" * 50)
+        # train
+        for idx_list, path_list in enumerate([train_path_list, val_path_list, test_path_list]):
+            
+            printlog(log_file, f"Processing cv{cv} data in split {idx_list}")
 
-        step1_path = pair["STEP1"]
-        step2_path = pair["STEP2"]
 
-        step1_file = nib.load(step1_path)
-        step2_file = nib.load(step2_path)
+            for pair in path_list:
+                
+                printlog(log_file, "<" * 50)
 
-        step1_data = step1_file.get_fdata()
-        step2_data = step2_file.get_fdata()
+                step1_path = pair["STEP1"]
+                step2_path = pair["STEP2"]
 
-        compress_step1_data = np.clip(step1_data, 0, 1).astype(np.float32)
-        compress_step1_path = step1_path.replace(".nii.gz", "_clip.nii.gz")
-        compress_step1_nii = nib.Nifti1Image(compress_step1_data, step1_file.affine, step1_file.header)
-        nib.save(compress_step1_nii, compress_step1_path)
-        print(f"Compressed data saved at {compress_step1_path}")
-        
-        print(f"Processing:  {step1_path} {step2_path}")
-        print(f"s1 shape: {step1_data.shape}, s2 shape: {step2_data.shape}")
-        print(f"s1 mean: {np.mean(step1_data):.4f}, s1 std: {np.std(step1_data):.4f}")
-        print(f"s2 mean: {np.mean(step2_data):.4f}, s2 std: {np.std(step2_data):.4f}")
-        print(f"s1 min: {np.min(step1_data):.4f}, s1 max: {np.max(step1_data):.4f}")
-        print(f"s2 min: {np.min(step2_data):.4f}, s2 max: {np.max(step2_data):.4f}")
-        print(f"s1 dtype: {step1_data.dtype}, s2 dtype: {step2_data.dtype}")
+                step1_file = nib.load(step1_path)
+                step2_file = nib.load(step2_path)
 
-        print(">" * 50)
+                step1_data = step1_file.get_fdata()
+                step2_data = step2_file.get_fdata()
+
+                compress_step1_data = np.clip(step1_data, 0, 1).astype(np.float32)
+                compress_step1_path = step1_path.replace(".nii.gz", "_clip.nii.gz")
+                compress_step1_nii = nib.Nifti1Image(compress_step1_data, step1_file.affine, step1_file.header)
+                nib.save(compress_step1_nii, compress_step1_path)
+                printlog(log_file, f"Compressed data saved at {compress_step1_path}")
+                
+                printlog(log_file, f"Processing:  {step1_path} {step2_path}")
+                printlog(log_file, f"s1 shape: {step1_data.shape}, s2 shape: {step2_data.shape} s3 shape: {compress_step1_data.shape}")
+                printlog(log_file, f"s1 mean: {np.mean(step1_data):.4f}, s1 std: {np.std(step1_data):.4f}")
+                printlog(log_file, f"s2 mean: {np.mean(step2_data):.4f}, s2 std: {np.std(step2_data):.4f}")
+                printlog(log_file, f"s3 mean: {np.mean(compress_step1_data):.4f}, s3 std: {np.std(compress_step1_data):.4f}")
+                printlog(log_file, f"s1 min: {np.min(step1_data):.4f}, s1 max: {np.max(step1_data):.4f}")
+                printlog(log_file, f"s2 min: {np.min(step2_data):.4f}, s2 max: {np.max(step2_data):.4f}")
+                printlog(log_file, f"s3 min: {np.min(compress_step1_data):.4f}, s3 max: {np.max(compress_step1_data):.4f}")
+                printlog(log_file, f"s1 dtype: {step1_data.dtype}, s2 dtype: {step2_data.dtype}, s3 dtype: {compress_step1_data.dtype}")
+
+                printlog(log_file, ">" * 50)
 
 
 
