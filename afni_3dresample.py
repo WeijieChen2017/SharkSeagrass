1714a0f2c7088958f2dcb934b712418609acc0b8
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-16 10:45:44 -0500

    Good morning!

---
+++ afni_3dresample.py
@@ -0,0 +1,10 @@
+target_folder = "B100/TOFNAC_CTACIVV_part2/"
+import os
+import json
+import glob
+
+data_to_convert = sorted(glob.glob("B100/TOFNAC_CTACIVV_part2/*.nii.gz"))
+for path in data_to_convert:
+    if not "_400" in path:
+        dst_path = path.replace(".nii.gz", "_256.nii.gz")
+        print(f"3dresample -dxyz 2.344 2.344 2.344 -prefix {dst_path} -inset {CT_path}")
