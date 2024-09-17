69e265031529b155c418f9b618815b179c0d8cc0
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-16 20:57:04 -0500

    Good morning!

--- UNetUNet_v1_run_step2.sh
+++ UNetUNet_v1_run_step2.sh
@@ -1,12 +1,15 @@
+echo "============================================"
+ls
+echo "============================================"
 unzip SharkSeagrass.zip
 rm SharkSeagrass.zip
 
 tar -xzvf TC256.tar.gz
-tar -xzvf cv{$1}_256_clip.tar.gz
+tar -xzvf cv${1}_256_clip.tar.gz
 rm TC256.tar.gz
 mv TC256 ./SharkSeagrass/
-rm cv{$1}_256_clip.tar.gz
-mv cv{$1}_256_clip ./SharkSeagrass/
+rm cv${1}_256_clip.tar.gz
+mv cv${1}_256_clip ./SharkSeagrass/
 mv d3f64_tsv1.pth ./SharkSeagrass/
 
 cd SharkSeagrass
@@ -22,18 +25,18 @@ ls
 # echo "============================================"
 # pip list
 echo "============================================"
-python UNetUNet_v1_py5_train.py --cross_validation $1
+python UNetUNet_v1_py5_step2.py --cross_validation $1
 echo "============================================"
 # python UNetUNet_v1_py2_train.py
 du -lh -d 1
 rm -r TC256
-rm -r cv{$1}_256
+rm -r cv${1}_256
 mv cache ./results/
-tar -czvf UNetUnet_256_cv{$1}_step2.tar.gz results
+tar -czvf UNetUnet_256_cv${1}_step2.tar.gz results
 echo "============================================"
 ls
 echo "============================================"
-mv UNetUnet_256_cv{$1}_step2.tar.gz ../
+mv UNetUnet_256_cv${1}_step2.tar.gz ../
 echo "============================================"
 ls
 echo "============================================"
