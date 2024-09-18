aa1a87b240717b7f9a1c6ff96787c711d5962805
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-18 17:16:05 -0500

    Good morning!

--- remove_bed.py
+++ remove_bed.py
@@ -1,11 +1,116 @@
 # z = 252
+
+# "E4143", 
+# "E4144", 
+# "E4147", 
+# "E4152", 
+# "E4155", 
+# "E4157", 
+# "E4158", 
+# "E4162", 
+# "E4163", 
+# "E4165", 
+# "E4166", 
+# "E4172", 
+# "E4181", 
+# "E4182", 
+# "E4183", 
+
 import nibabel as nib
 import numpy as np
 import glob
 import os
+import random
+import string
+
+MID_PET = 5000
+MIQ_PET = 0.9
+MAX_PET = 20000
+MAX_CT = 1976
+MIN_CT = -1024
+MIN_PET = 0
+RANGE_CT = MAX_CT - MIN_CT
+RANGE_PET = MAX_PET - MIN_PET
+
+def get_random_name():
+    # we need to use combination of letters from A-Z and return a 3 letter string
+    return ''.join(random.choices(string.ascii_uppercase, k=3))
+
+
+def two_segment_scale(arr, MIN, MID, MAX, MIQ):
+    # Create an empty array to hold the scaled results
+    scaled_arr = np.zeros_like(arr, dtype=np.float32)
+
+    # First segment: where arr <= MID
+    mask1 = arr <= MID
+    scaled_arr[mask1] = (arr[mask1] - MIN) / (MID - MIN) * MIQ
+
+    # Second segment: where arr > MID
+    mask2 = arr > MID
+    scaled_arr[mask2] = MIQ + (arr[mask2] - MID) / (MAX - MID) * (1 - MIQ)
+    
+    return scaled_arr
+
+mask = nib.load("B100/TOFNAC_CTACIVV_part2/CTAC_bed.nii.gz")
+mask_data = mask.get_fdata()[21:277, 21:277, 252]
+mask_data = 1 - mask_data
+save_folder = "B100/TOFNAC_CTACIVV_part2/TC256_part2_norm/"
+if not os.path.exists(save_folder):
+    os.makedirs(save_folder)
+# this is the bed mask for 0/1 masking
 
 data_folder = "B100/TOFNAC_CTACIVV_part2/TC256_part2/"
 case_list = sorted(glob.glob(data_folder + "CTACIVV_*_256.nii.gz"))
 for case in case_list:
     case_tag = os.path.basename(case).split("_")[1]
-    print(f"\"{case_tag}\", ")
+    # print(f"\"{case_tag}\", ")
+    print("> " * 50)
+    TOFNAC_path = case.replace("CTACIVV", "TOFNAC")
+    CTACIVV_path = case
+
+    TOFNAC_file = nib.load(TOFNAC_path)
+    CTACIVV_file = nib.load(CTACIVV_path)
+
+    TOFNAC_data = TOFNAC_file.get_fdata()
+    CTACIVV_data = CTACIVV_file.get_fdata()
+
+    TOFNAC_data = TOFNAC_data.astype(np.float32)
+    CTACIVV_data = CTACIVV_data.astype(np.float32)# from 299 to 256
+    CTACIVV_data = CTACIVV_data[21:277, 21:277, :]
+    print(f"TOFNAC: {TOFNAC_data.shape}, CTACIVV: {CTACIVV_data.shape}")
+    print(f"TOFNAC MAX: {TOFNAC_data.max()}, MIN: {TOFNAC_data.min()}")
+    print(f"CTACIVV MAX: {CTACIVV_data.max()}, MIN: {CTACIVV_data.min()}")
+    print(f"TOFNAC MEAN: {TOFNAC_data.mean()}, STD: {TOFNAC_data.std()}")
+    print(f"CTACIVV MEAN: {CTACIVV_data.mean()}, STD: {CTACIVV_data.std()}")
+
+    # clip
+    TOFNAC_data = np.clip(TOFNAC_data, MIN_PET, MAX_PET)
+    CTACIVV_data = np.clip(CTACIVV_data, MIN_CT, MAX_CT)
+
+    TOFNAC_data = two_segment_scale(TOFNAC_data, MIN_CT, MID_PET, MAX_CT, MIQ_PET)
+    CTACIVV_data = (CTACIVV_data - MIN_CT) / RANGE_CT
+
+    # masking, the mask part should be set to 0, while the mask is 1
+    len_z = TOFNAC_data.shape[2]
+    for z in range(len_z):
+        CTACIVV_data[:, :, z] = CTACIVV_data[:, :, z] * mask_data
+
+    new_name = get_random_name() + case_tag[2:]
+    new_path_TOFNAC = os.path.join(save_folder, f"{new_name}_TOFNAC_256.nii.gz")
+    new_path_CTACIVV = os.path.join(save_folder, f"{new_name}_CTAC_256.nii.gz")
+
+    new_file_TOFNAC = nib.Nifti1Image(TOFNAC_data, TOFNAC_file.affine, TOFNAC_file.header)
+    new_file_CTACIVV = nib.Nifti1Image(CTACIVV_data, CTACIVV_file.affine, CTACIVV_file.header)
+
+    nib.save(new_file_TOFNAC, new_path_TOFNAC)
+    nib.save(new_file_CTACIVV, new_path_CTACIVV)
+    print(f"Saved: {new_path_TOFNAC}")
+    print(f"Saved: {new_path_CTACIVV}")
+
+    
+    
+
+
+
+
+
