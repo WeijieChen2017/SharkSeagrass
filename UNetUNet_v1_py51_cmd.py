385e578c7670406a1eca789b8791baeba46a62ec
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-18 10:44:02 -0500

    Good morning!

--- UNetUNet_v1_py51_cmd.py
+++ UNetUNet_v1_py51_cmd.py
@@ -1,12 +1,12 @@
 import os
 
-# cmd_1 = "python UNetUNet_v1_py5_step6_eval.py -c 0"
+cmd_1 = "python UNetUNet_v1_py5_step6_eval.py -c 0"
 cmd_2 = "python UNetUNet_v1_py5_step6_eval.py -c 1"
 cmd_3 = "python UNetUNet_v1_py5_step6_eval.py -c 2"
 cmd_4 = "python UNetUNet_v1_py5_step6_eval.py -c 3"
 cmd_5 = "python UNetUNet_v1_py5_step6_eval.py -c 4"
 
-for cmd in [cmd_2, cmd_3, cmd_4, cmd_5]:
+for cmd in [cmd_1, cmd_2, cmd_3, cmd_4, cmd_5]:
     print("<<"*20)
     print(cmd)
     os.system(cmd)
