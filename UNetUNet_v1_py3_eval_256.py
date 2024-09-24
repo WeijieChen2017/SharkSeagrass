0012a2e618e3a5a93d12bd1ebc7f50263722e5a8
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-24 12:21:11 -0500

    Good morning!

--- UNetUNet_v1_py3_eval_256.py
+++ UNetUNet_v1_py3_eval_256.py
@@ -254,12 +254,15 @@ def main():
                         data_x = TOFNAC_data[:, idx_y-1:idx_y+2, :]
                     # data_x is 256, 3, 720
                     # convert it to 1, 3, 720, 256
+                    print(data_x.shape)
                     data_x = np.transpose(data_x, (1, 2, 0))
                     data_x = np.expand_dims(data_x, axis=0)
                     data_x = torch.tensor(data_x, dtype=torch.float32).to(device)
+                    print(data_x.shape)
                     with torch.no_grad():
                         pred_y = model(data_x)
                         pred_y = pred_y.cpu().detach().numpy()
+                        print(pred_y.shape)
                         pred_y = np.squeeze(pred_y, axis=0) # 720, 256
                         print(pred_y.shape)
                         pred_y = np.transpose(pred_y, (1, 0)) # 256, 720
