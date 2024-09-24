c50e485a889f5064d98ba642910e7b333b6d01fb
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-24 12:11:37 -0500

    Good morning!

--- UNetUNet_v1_py3_eval_256.py
+++ UNetUNet_v1_py3_eval_256.py
@@ -19,7 +19,7 @@ from UNetUNet_v1_py2_train_util import VQModel, simple_logger, prepare_dataset
 MID_PET = 5000
 MIQ_PET = 0.9
 MAX_PET = 20000
-MAX_CT = 1976
+MAX_CT = 2976
 MIN_CT = -1024
 MIN_PET = 0
 RANGE_CT = MAX_CT - MIN_CT
@@ -44,6 +44,7 @@ def main():
     # here I will use argparse to parse the arguments
     argparser = argparse.ArgumentParser(description='Prepare dataset for training')
     argparser.add_argument('-c', '--cross_validation', type=int, default=0, help='Index of the cross validation')
+    argparser.add_argument('--acs', type=bool, default=True, help='Whether to run in a/c/s three directions')
     argparser.add_argument('-p', '--part2', type=bool, default=False, help='Whether to run the second part')
     args = argparser.parse_args()
     tag = f"fold{args.cross_validation}_256"
@@ -156,7 +157,194 @@ def main():
     print("Model step 1 loaded from", model_step1_params["ckpt_path"])
     model.to(device)
 
-    if not args.part2:
+    if args.acs:
+        data_folder = "B100/TC256/"
+        data_split = ["train", "val", "test"]
+        # load json
+        with open(data_div_json, "r") as f:
+            data_div = json.load(f)
+        data_div_cv = data_div[f"cv_{cross_validation}"]
+
+        log_file = os.path.join(root_folder, "log.txt")
+        with open(log_file, "w") as f:
+            print(f"Training division: {data_div_cv['train']}")
+            print(f"Validation division: {data_div_cv['val']}")
+            print(f"Testing division: {data_div_cv['test']}")
+
+        for split in data_split:
+            data_split_folder = os.path.join(root_folder, split)
+            if not os.path.exists(data_split_folder):
+                os.makedirs(data_split_folder)
+            
+            split_list = data_div_cv[split]
+            split_loss_axial = []
+            split_loss_coronal = []
+            split_loss_sagittal = []
+            split_loss_average = []
+
+            # now there will be a list of file names
+            for casename in split_list:
+                print(f"{split} -> Processing {casename}")
+                # casenme "FGX078"
+                # TOFNAC_path "FGX078_CTAC.nii.gz" have been normalized
+                # CTAC_path "FGX078_TOFNAC.nii.gz" have been normalized
+                TOFNAC_path = os.path.join(data_folder, f"{casename}_TOFNAC_256.nii.gz")
+                CTAC_path = os.path.join(data_folder, f"{casename}_CTAC_256.nii.gz")
+                # load the data
+                TOFNAC_file = nib.load(TOFNAC_path)
+                CTAC_file = nib.load(CTAC_path)
+
+                TOFNAC_data = TOFNAC_file.get_fdata()
+                CTAC_data = CTAC_file.get_fdata()
+                print(f"{split} -> {casename} -> TOFNAC shape: {TOFNAC_data.shape}, CTAC shape: {CTAC_data.shape}")
+
+                CTAC_pred_axial = np.zeros_like(CTAC_data)
+                CTAC_pred_coronal = np.zeros_like(CTAC_data)
+                CTAC_pred_sagittal = np.zeros_like(CTAC_data)
+
+                # for axial
+                for idx_z in range(TOFNAC_data.shape[2]):
+                    if idx_z == 0:
+                        slice_1 = TOFNAC_data[:, :, idx_z].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_2 = TOFNAC_data[:, :, idx_z].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_3 = TOFNAC_data[:, :, idx_z+1].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        data_x = np.concatenate([slice_1, slice_2, slice_3], axis=2)
+                    elif idx_z == TOFNAC_data.shape[2] - 1:
+                        slice_1 = TOFNAC_data[:, :, idx_z-1].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_2 = TOFNAC_data[:, :, idx_z].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_3 = TOFNAC_data[:, :, idx_z].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        data_x = np.concatenate([slice_1, slice_2, slice_3], axis=2)
+                    else:
+                        data_x = TOFNAC_data[:, :, idx_z-1:idx_z+2]
+                    # data_x is 400x400x3, convert it to 1x3x400x400
+                    data_x = np.transpose(data_x, (2, 0, 1))
+                    data_x = np.expand_dims(data_x, axis=0)
+                    data_x = torch.tensor(data_x, dtype=torch.float32).to(device)
+                    with torch.no_grad():
+                        pred_y = model(data_x)
+                        pred_y = pred_y.cpu().detach().numpy()
+                        pred_y = np.squeeze(pred_y, axis=0)
+                        CTAC_pred_axial[:, :, idx_z] = pred_y
+
+                # for coronal
+                for idx_y in range(TOFNAC_data.shape[1]):
+                    if idx_y == 0:
+                        slice_1 = TOFNAC_data[:, idx_y, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_2 = TOFNAC_data[:, idx_y, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_3 = TOFNAC_data[:, idx_y, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        data_x = np.concatenate([slice_1, slice_2, slice_3], axis=2)
+                    elif idx_y == TOFNAC_data.shape[1] - 1:
+                        slice_1 = TOFNAC_data[:, idx_y-1, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_2 = TOFNAC_data[:, idx_y, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_3 = TOFNAC_data[:, idx_y, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        data_x = np.concatenate([slice_1, slice_2, slice_3], axis=2)
+                    else:
+                        data_x = TOFNAC_data[:, idx_y-1:idx_y+2, :]
+                    # data_x is 256, 3, 720
+                    # convert it to 1, 3, 720, 256
+                    data_x = np.transpose(data_x, (1, 2, 0))
+                    data_x = np.expand_dims(data_x, axis=0)
+                    data_x = torch.tensor(data_x, dtype=torch.float32).to(device)
+                    with torch.no_grad():
+                        pred_y = model(data_x)
+                        pred_y = pred_y.cpu().detach().numpy()
+                        pred_y = np.squeeze(pred_y, axis=0) # 720, 256
+                        pred_y = np.transpose(pred_y, (1, 0)) # 256, 720
+                        CTAC_pred_coronal[:, idx_y, :] = pred_y
+                
+                # for sagittal
+                for idx_x in range(TOFNAC_data.shape[0]):
+                    if idx_x == 0:
+                        slice_1 = TOFNAC_data[idx_x, :, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_2 = TOFNAC_data[idx_x, :, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_3 = TOFNAC_data[idx_x, :, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        data_x = np.concatenate([slice_1, slice_2, slice_3], axis=2)
+                    elif idx_x == TOFNAC_data.shape[0] - 1:
+                        slice_1 = TOFNAC_data[idx_x-1, :, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_2 = TOFNAC_data[idx_x, :, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        slice_3 = TOFNAC_data[idx_x, :, :].reshape(model_step1_params["img_size"], model_step1_params["img_size"], 1)
+                        data_x = np.concatenate([slice_1, slice_2, slice_3], axis=2)
+                    else:
+                        data_x = TOFNAC_data[idx_x-1:idx_x+2, :, :]
+                    # data_x is 3, 256, 720
+                    # convert it to 1, 3, 720, 256
+                    data_x = np.transpose(data_x, (0, 2, 1))
+                    data_x = np.expand_dims(data_x, axis=0)
+                    data_x = torch.tensor(data_x, dtype=torch.float32).to(device)
+                    with torch.no_grad():
+                        pred_y = model(data_x)
+                        pred_y = pred_y.cpu().detach().numpy()
+                        pred_y = np.squeeze(pred_y, axis=0)
+                        pred_y = np.transpose(pred_y, (1, 0))
+                        CTAC_pred_sagittal[idx_x, :, :] = pred_y
+                
+                CTAC_pred_average = (CTAC_pred_axial + CTAC_pred_coronal + CTAC_pred_sagittal) / 3
+                # save the CTAC_pred
+                CTAC_pred_axial = np.clip(CTAC_pred_axial, 0, 1).astype(np.float32)
+                CTAC_pred_coronal = np.clip(CTAC_pred_coronal, 0, 1).astype(np.float32)
+                CTAC_pred_sagittal = np.clip(CTAC_pred_sagittal, 0, 1).astype(np.float32)
+                CTAC_pred_average = np.clip(CTAC_pred_average, 0, 1).astype(np.float32)
+
+                CTAC_pred_axial_path = os.path.join(data_split_folder, f"{casename}_CTAC_pred_axial_cv{cross_validation}.nii.gz")
+                CTAC_pred_coronal_path = os.path.join(data_split_folder, f"{casename}_CTAC_pred_coronal_cv{cross_validation}.nii.gz")
+                CTAC_pred_sagittal_path = os.path.join(data_split_folder, f"{casename}_CTAC_pred_sagittal_cv{cross_validation}.nii.gz")
+                CTAC_pred_average_path = os.path.join(data_split_folder, f"{casename}_CTAC_pred_average_cv{cross_validation}.nii.gz")
+
+                # renorm
+                CTAC_pred_axial = CTAC_pred_axial * data_loader_params["norm"]["RANGE_CT"] + data_loader_params["norm"]["MIN_CT"]
+                CTAC_pred_coronal = CTAC_pred_coronal * data_loader_params["norm"]["RANGE_CT"] + data_loader_params["norm"]["MIN_CT"]
+                CTAC_pred_sagittal = CTAC_pred_sagittal * data_loader_params["norm"]["RANGE_CT"] + data_loader_params["norm"]["MIN_CT"]
+                CTAC_pred_average = CTAC_pred_average * data_loader_params["norm"]["RANGE_CT"] + data_loader_params["norm"]["MIN_CT"]
+                
+                # save the .nii.gz
+                CTAC_pred_axial_nii = nib.Nifti1Image(CTAC_pred_axial, CTAC_file.affine, CTAC_file.header)
+                CTAC_pred_coronal_nii = nib.Nifti1Image(CTAC_pred_coronal, CTAC_file.affine, CTAC_file.header)
+                CTAC_pred_sagittal_nii = nib.Nifti1Image(CTAC_pred_sagittal, CTAC_file.affine, CTAC_file.header)
+                CTAC_pred_average_nii = nib.Nifti1Image(CTAC_pred_average, CTAC_file.affine, CTAC_file.header)
+                
+                nib.save(CTAC_pred_axial_nii, CTAC_pred_axial_path)
+                nib.save(CTAC_pred_coronal_nii, CTAC_pred_coronal_path)
+                nib.save(CTAC_pred_sagittal_nii, CTAC_pred_sagittal_path)
+                nib.save(CTAC_pred_average_nii, CTAC_pred_average_path)
+
+                print(f"Save the CTAC_pred_axial to {CTAC_pred_axial_path}")
+                print(f"Save the CTAC_pred_coronal to {CTAC_pred_coronal_path}")
+                print(f"Save the CTAC_pred_sagittal to {CTAC_pred_sagittal_path}")
+                print(f"Save the CTAC_pred_average to {CTAC_pred_average_path}")
+                
+                # compute the loss
+                CTAC_HU = CTAC_data * data_loader_params["norm"]["RANGE_CT"] + data_loader_params["norm"]["MIN_CT"]
+                CTAC_mask = CTAC_HU > -500
+                MAE_axial = np.mean(np.abs(CTAC_HU[CTAC_mask] - CTAC_pred_axial[CTAC_mask]))
+                MAE_coronal = np.mean(np.abs(CTAC_HU[CTAC_mask] - CTAC_pred_coronal[CTAC_mask]))
+                MAE_sagittal = np.mean(np.abs(CTAC_HU[CTAC_mask] - CTAC_pred_sagittal[CTAC_mask]))
+                MAE_average = np.mean(np.abs(CTAC_HU[CTAC_mask] - CTAC_pred_average[CTAC_mask]))
+                print(f"{split} -> {casename} -> MAE_axial: {MAE_axial:.4f}, MAE_coronal: {MAE_coronal:.4f}, MAE_sagittal: {MAE_sagittal:.4f}, MAE_average: {MAE_average:.4f}")
+                split_loss_axial.append(MAE_axial)
+                split_loss_coronal.append(MAE_coronal)
+                split_loss_sagittal.append(MAE_sagittal)
+                split_loss_average.append(MAE_average)
+
+                with open(log_file, "a") as f:
+                    f.write(f"{split} -> {casename} -> MAE_axial: {MAE_axial:.4f}, MAE_coronal: {MAE_coronal:.4f}, MAE_sagittal: {MAE_sagittal:.4f}, MAE_average: {MAE_average:.4f}\n")
+            
+            split_loss_axial = np.array(split_loss_axial)
+            split_loss_coronal = np.array(split_loss_coronal)
+            split_loss_sagittal = np.array(split_loss_sagittal)
+            split_loss_average = np.array(split_loss_average)
+
+            split_loss_average = np.mean(split_loss_average)
+            split_loss_axial = np.mean(split_loss_axial)
+            split_loss_coronal = np.mean(split_loss_coronal)
+            split_loss_sagittal = np.mean(split_loss_sagittal)
+
+            print(f"{split} -> Average MAE_axial: {split_loss_axial:.4f}, MAE_coronal: {split_loss_coronal:.4f}, MAE_sagittal: {split_loss_sagittal:.4f}, MAE_average: {split_loss_average:.4f}")
+            with open(log_file, "a") as f:
+                f.write(f"{split} -> Average MAE_axial: {split_loss_axial:.4f}, MAE_coronal: {split_loss_coronal:.4f}, MAE_sagittal: {split_loss_sagittal:.4f}, MAE_average: {split_loss_average:.4f}\n")
+
+        print("Done!")
+    
+    elif not args.part2:
         data_folder = "B100/TC256/"
         data_split = ["train", "val", "test"]
         # load json
