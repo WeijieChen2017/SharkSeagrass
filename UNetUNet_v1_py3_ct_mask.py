c835a529824523b34b39ff5624f73eed1d0e953a
WeijieChen2017 <weijiechen1994@gmail.com>
2024-09-25 14:25:22 -0500

    Good morning!

---
+++ UNetUNet_v1_py3_ct_mask.py
@@ -0,0 +1,246 @@
+import os
+
+# set the environment variable to use the GPU if available
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import torch
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("The device is: ", device)
+
+import argparse
+import json
+import time
+import random
+import nibabel as nib
+import numpy as np
+
+from UNetUNet_v1_py2_train_util import VQModel, simple_logger, prepare_dataset
+
+from scipy.ndimage import binary_fill_holes
+
+MIN_CT = -1024
+MAX_CT = 2976
+RANGE_CT = MAX_CT - MIN_CT
+WRONG_MAX_CT = 1976
+WRONG_RANGE_CT = WRONG_MAX_CT - MIN_CT
+
+def correct_CT_range(DLCTAC_data):
+    DLCTAC_data = (DLCTAC_data - MIN_CT) / WRONG_RANGE_CT
+    DLCTAC_data = DLCTAC_data * RANGE_CT + MIN_CT
+    return DLCTAC_data
+
+def two_segment_scale(arr, MIN, MID, MAX, MIQ):
+    # Create an empty array to hold the scaled results
+    scaled_arr = np.zeros_like(arr, dtype=np.float32)
+
+    # First segment: where arr <= MID
+    mask1 = arr <= MID
+    scaled_arr[mask1] = (arr[mask1] - MIN) / (MID - MIN) * MIQ
+
+    # Second segment: where arr > MID
+    mask2 = arr > MID
+    scaled_arr[mask2] = MIQ + (arr[mask2] - MID) / (MAX - MID) * (1 - MIQ)
+    
+    return scaled_arr
+
+def main():
+    # here I will use argparse to parse the arguments
+    argparser = argparse.ArgumentParser(description='Prepare dataset for training')
+    argparser.add_argument('-c', '--cross_validation', type=int, default=0, help='Index of the cross validation')
+    argparser.add_argument('--acs', type=bool, default=True, help='Whether to run in a/c/s three directions')
+    argparser.add_argument('-p', '--part2', type=bool, default=False, help='Whether to run the second part')
+    args = argparser.parse_args()
+    tag = f"fold{args.cross_validation}_256"
+
+    random_seed = 42
+    # set the random seed
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    cross_validation = args.cross_validation
+    # root_folder = f"B100/UNetUNet_best/cv{cross_validation}_256/"
+    root_folder = f"results/cv{cross_validation}_256/"
+    data_div_json = "UNetUNet_v1_data_split.json"
+    if not os.path.exists(root_folder):
+        os.makedirs(root_folder)
+    print("The root folder is: ", root_folder)
+
+    data_loader_params = {
+        "norm": {
+            "MID_PET": 5000,
+            "MIQ_PET": 0.9,
+            "MAX_PET": 20000,
+            "MAX_CT": 1976,
+            "MIN_CT": -1024,
+            "MIN_PET": 0,
+            "RANGE_CT": 3000,
+            "RANGE_PET": 20000,
+        },
+        "train": {
+            "batch_size": 1,
+            "shuffle": True,
+            "num_workers_cache": 4,
+            "num_workers_loader": 8,
+            "cache_rate": 0.5,
+        },
+        "val": {
+            "batch_size": 1,
+            "shuffle": False,
+            "num_workers_cache": 2,
+            "num_workers_loader": 4,
+            "cache_rate": 1.0,
+        },
+        "test": {
+            "batch_size": 1,
+            "shuffle": False,
+            "num_workers_cache": 1,
+            "num_workers_loader": 2,
+            "cache_rate": 1.0,
+        },
+    }
+
+    model_step1_params = {
+        "VQ_NAME": "f4-noattn",
+        "n_embed": 8192,
+        "embed_dim": 3,
+        "img_size" : 256,
+        "input_modality" : ["TOFNAC", "CTAC"],
+        # "ckpt_path": f"B100/TC256_best_ckpt/best_model_cv{cross_validation}.pth",
+        "ckpt_path": root_folder+f"best_model_cv{cross_validation}.pth",
+        "ddconfig": {
+            "attn_type": "none",
+            "double_z": False,
+            "z_channels": 3,
+            "resolution": 256,
+            "in_channels": 3,
+            "out_ch": 1,
+            "ch": 128,
+            "ch_mult": [1, 2, 4],
+            "num_res_blocks": 2,
+            "attn_resolutions": [],
+            "dropout": 0.0,
+        }
+    }
+
+    global_config = {}
+
+    # initialize wandb
+    global_config["tag"] = tag
+    global_config["input_modality"] = ["TOFNAC", "CTAC"]
+    global_config["model_step1_params"] = model_step1_params
+    global_config["data_loader_params"] = data_loader_params
+    # global_config["train_params"] = train_params
+    global_config["cross_validation"] = args.cross_validation
+
+    
+    global_config["root_folder"] = root_folder
+
+    if args.acs:
+        data_folder = "B100/TC256/"
+        data_split = ["test", "train", "val"]
+        # load json
+        with open(data_div_json, "r") as f:
+            data_div = json.load(f)
+        data_div_cv = data_div[f"cv_{cross_validation}"]
+
+        log_file = os.path.join(root_folder, "log.txt")
+        with open(log_file, "w") as f:
+            print(f"Training division: {data_div_cv['train']}")
+            print(f"Validation division: {data_div_cv['val']}")
+            print(f"Testing division: {data_div_cv['test']}")
+
+        for split in data_split:
+            mask_save_folder = os.path.join(root_folder, split+"_mask")
+            if not os.path.exists(mask_save_folder):
+                os.makedirs(mask_save_folder)
+            
+                CTAC_pred_axial_path = os.path.join(data_split_folder, f"{casename}_CTAC_pred_axial_cv{cross_validation}.nii.gz")
+                CTAC_pred_coronal_path = os.path.join(data_split_folder, f"{casename}_CTAC_pred_coronal_cv{cross_validation}.nii.gz")
+                CTAC_pred_sagittal_path = os.path.join(data_split_folder, f"{casename}_CTAC_pred_sagittal_cv{cross_validation}.nii.gz")
+                CTAC_pred_average_path = os.path.join(data_split_folder, f"{casename}_CTAC_pred_average_cv{cross_validation}.nii.gz")
+                CTAC_pred_median_path = os.path.join(data_split_folder, f"{casename}_CTAC_pred_median_cv{cross_validation}.nii.gz")
+
+                CT_pred_axial_file = nib.load(CT_pred_axial_path)
+                CT_pred_coronal_file = nib.load(CT_pred_coronal_path)
+                CT_pred_sagittal_file = nib.load(CT_pred_sagittal_path)
+                CT_pred_average_file = nib.load(CT_pred_average_path)
+                CT_pred_median_file = nib.load(CT_pred_median_path)
+
+                CT_pred_axial_data = CT_pred_axial_file.get_fdata()
+                CT_pred_coronal_data = CT_pred_coronal_file.get_fdata()
+                CT_pred_sagittal_data = CT_pred_sagittal_file.get_fdata()
+                CT_pred_average_data = CT_pred_average_file.get_fdata()
+                CT_pred_median_data = CT_pred_median_file.get_fdata()
+                
+                # adjust the range of the CT data
+                CT_pred_axial_data = correct_CT_range(CT_pred_axial_data)
+                CT_pred_coronal_data = correct_CT_range(CT_pred_coronal_data)
+                CT_pred_sagittal_data = correct_CT_range(CT_pred_sagittal_data)
+                CT_pred_average_data = correct_CT_range(CT_pred_average_data)
+                CT_pred_median_data = correct_CT_range(CT_pred_median_data)
+
+                # save the data
+                CTAC_pred_axial_rescale_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_axial_rescale_cv{cross_validation}.nii.gz")
+                CTAC_pred_coronal_rescale_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_coronal_rescale_cv{cross_validation}.nii.gz")
+                CTAC_pred_sagittal_rescale_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_sagittal_rescale_cv{cross_validation}.nii.gz")
+                CTAC_pred_average_rescale_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_average_rescale_cv{cross_validation}.nii.gz")
+                CTAC_pred_median_rescale_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_median_rescale_cv{cross_validation}.nii.gz")
+
+                CTAC_pred_axial_rescale_file = nib.Nifti1Image(CT_pred_axial_data, CT_pred_axial_file.affine, CT_pred_axial_file.header)
+                CTAC_pred_coronal_rescale_file = nib.Nifti1Image(CT_pred_coronal_data, CT_pred_coronal_file.affine, CT_pred_coronal_file.header)
+                CTAC_pred_sagittal_rescale_file = nib.Nifti1Image(CT_pred_sagittal_data, CT_pred_sagittal_file.affine, CT_pred_sagittal_file.header)
+                CTAC_pred_average_rescale_file = nib.Nifti1Image(CT_pred_average_data, CT_pred_average_file.affine, CT_pred_average_file.header)
+                CTAC_pred_median_rescale_file = nib.Nifti1Image(CT_pred_median_data, CT_pred_median_file.affine, CT_pred_median_file.header)
+
+                nib.save(CTAC_pred_axial_rescale_file, CTAC_pred_axial_rescale_path)
+                nib.save(CTAC_pred_coronal_rescale_file, CTAC_pred_coronal_rescale_path)
+                nib.save(CTAC_pred_sagittal_rescale_file, CTAC_pred_sagittal_rescale_path)
+                nib.save(CTAC_pred_average_rescale_file, CTAC_pred_average_rescale_path)
+                nib.save(CTAC_pred_median_rescale_file, CTAC_pred_median_rescale_path)
+                print(f"Axial, coronal, sagittal, average, and median masks saved for {casename}")
+
+                # compute the mask
+                axial_mask = CT_pred_axial_data > -500
+                coronal_mask = CT_pred_coronal_data > -500
+                sagittal_mask = CT_pred_sagittal_data > -500
+                average_mask = CT_pred_average_data > -500
+                median_mask = CT_pred_median_data > -500
+
+                # fill holes
+                for z in range(CT_pred_axial_data.shape[2]):
+                    axial_mask[:, :, z] = binary_fill_holes(axial_mask[:, :, z])
+                    coronal_mask[:, :, z] = binary_fill_holes(coronal_mask[:, :, z])
+                    sagittal_mask[:, :, z] = binary_fill_holes(sagittal_mask[:, :, z])
+                    average_mask[:, :, z] = binary_fill_holes(average_mask[:, :, z])
+                    median_mask[:, :, z] = binary_fill_holes(median_mask[:, :, z])
+
+                # save the mask
+                CTAC_pred_axial_mask_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_axial_mask_cv{cross_validation}.nii.gz")
+                CTAC_pred_coronal_mask_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_coronal_mask_cv{cross_validation}.nii.gz")
+                CTAC_pred_sagittal_mask_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_sagittal_mask_cv{cross_validation}.nii.gz")
+                CTAC_pred_average_mask_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_average_mask_cv{cross_validation}.nii.gz")
+                CTAC_pred_median_mask_path = os.path.join(mask_save_folder, f"{casename}_CTAC_pred_median_mask_cv{cross_validation}.nii.gz")
+
+                CTAC_pred_axial_mask_file = nib.Nifti1Image(axial_mask, CT_pred_axial_file.affine, CT_pred_axial_file.header)
+                CTAC_pred_coronal_mask_file = nib.Nifti1Image(coronal_mask, CT_pred_coronal_file.affine, CT_pred_coronal_file.header)
+                CTAC_pred_sagittal_mask_file = nib.Nifti1Image(sagittal_mask, CT_pred_sagittal_file.affine, CT_pred_sagittal_file.header)
+                CTAC_pred_average_mask_file = nib.Nifti1Image(average_mask, CT_pred_average_file.affine, CT_pred_average_file.header)
+                CTAC_pred_median_mask_file = nib.Nifti1Image(median_mask, CT_pred_median_file.affine, CT_pred_median_file.header)
+
+                nib.save(CTAC_pred_axial_mask_file, CTAC_pred_axial_mask_path)
+                nib.save(CTAC_pred_coronal_mask_file, CTAC_pred_coronal_mask_path)
+                nib.save(CTAC_pred_sagittal_mask_file, CTAC_pred_sagittal_mask_path)
+                nib.save(CTAC_pred_average_mask_file, CTAC_pred_average_mask_path)
+                nib.save(CTAC_pred_median_mask_file, CTAC_pred_median_mask_path)
+
+                print(f"Axial, coronal, sagittal, average, and median masks saved for {casename}")
+
+        print("Done!")
+    
+if __name__ == "__main__":
+    main()
---
+++ UNetUNet_v1_py6_recon123 copy.py
@@ -0,0 +1,191 @@
+TOFNAC_data_folder = "B100/TOFNAC/"
+CTAC_data_folder = "B100/CTACIVV/"
+CTAC_bed_folder = "B100/CTAC_bed/"
+CTAC_resample_folder = "B100/CTACIVV_resample/"
+TC256_folder = "B100/TC256/"
+DLCTAC_folder = "B100/DLCTAC/"
+DLCTAC_bed_fillholes_folder = "B100/DLCTAC_bed/"
+pred_folder = "B100/UNetUnet_best/test/"
+
+import os
+import glob
+import nibabel as nib
+import numpy as np
+from scipy.ndimage import binary_fill_holes
+
+tag_list = [
+    "E4055", "E4058", "E4061",          "E4066",
+    "E4068", "E4069", "E4073", "E4074", "E4077",
+    "E4078", "E4079",          "E4081", "E4084",
+             "E4091", "E4092", "E4094", "E4096",
+             "E4098", "E4099",          "E4103",
+    "E4105", "E4106", "E4114", "E4115", "E4118",
+    "E4120", "E4124", "E4125", "E4128", "E4129",
+    "E4130", "E4131", "E4134", "E4137", "E4138",
+    "E4139",
+]
+
+MIN_CT = -1024
+MAX_CT = 2976
+RANGE_CT = MAX_CT - MIN_CT
+WRONG_MAX_CT = 1976
+WRONG_RANGE_CT = WRONG_MAX_CT - MIN_CT
+
+# save_folder = "B100/DLCTAC_bed/"
+# if not os.path.exists(save_folder):
+#     os.makedirs(save_folder)
+
+save_folder = "B100/ForCatilin/"
+save_folder_TOFNAC = save_folder + "TOFNAC/"
+save_folder_CTAC = save_folder + "CTAC/"
+save_folder_DLCT = save_folder + "DLCT/"
+for folder in [save_folder_TOFNAC, save_folder_CTAC, save_folder_DLCT]:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+# for tag in tag_list:
+#     CTAC_path = f"{CTAC_data_folder}CTACIVV_{tag[1:]}_256.nii.gz"
+#     CTAC_file = nib.load(CTAC_path)
+#     CTAC_data = CTAC_file.get_fdata()
+
+#     # CTAC_resample_path = f"{CTAC_resample_folder}CTACIVV_{tag[1:]}.nii.gz"
+#     # check if the file exists
+#     # if not os.path.exists(CTAC_resample_path):
+#     #     print(f"CTAC file not found for {tag}")
+#         # continue
+#     # TC256_path = glob.glob(os.path.join(TC256_folder, f"*{tag[3:]}_CTAC_256.nii.gz"))[0]
+#     pred_path = glob.glob(os.path.join(pred_folder, f"*{tag[2:]}_CTAC_pred*.nii.gz"))[0]
+#     # print(f"TC256_path: {TC256_path}")
+#     # print(f"pred_path: {pred_path}")
+#     # print(f"{len(pred_path)} files found for {tag}, using {pred_path}")
+
+#     # CTAC_resample_file = nib.load(CTAC_resample_path)
+#     pred_file = nib.load(pred_path)
+
+#     # CTAC_resample_data = CTAC_resample_file.get_fdata()
+#     pred_data = pred_file.get_fdata()
+
+#     print("<" * 50)
+#     print(f"Processing {tag}")
+#     print(f"CTAC path: {CTAC_path}")
+#     print(f"pred path: {pred_path}")
+#     print(f"CTAC shape: {CTAC_data.shape}")
+#     print(f"pred shape: {pred_data.shape}")
+
+
+#     # pad to CTAC size
+#     full_data = np.zeros(CTAC_data.shape, dtype=np.float32)
+#     if CTAC_data.shape[2] == pred_data.shape[2]:
+#         full_data[21:277, 21:277, :] = pred_data
+#     elif CTAC_data.shape[2] < pred_data.shape[2]:
+#         len_diff = pred_data.shape[2] - CTAC_data.shape[2]
+#         full_data[21:277, 21:277, :] = pred_data[:, :, :CTAC_data.shape[2]]
+#     elif CTAC_data.shape[2] > pred_data.shape[2]:
+#         len_diff = CTAC_data.shape[2] - pred_data.shape[2]
+#         full_data[21:277, 21:277, :pred_data.shape[2]] = pred_data
+#     full_data = np.clip(full_data, 0, 1)
+
+#     # rescale the data
+#     full_data = full_data * RANGE_CT + MIN_CT
+    
+#     # save the data
+#     save_path = os.path.join(save_folder, f"E4{tag[2:]}_CTAC_DL.nii.gz")
+#     save_nii = nib.Nifti1Image(full_data, CTAC_file.affine, CTAC_file.header)
+#     nib.save(save_nii, save_path)
+#     print(f"Data saved at {save_path}")
+
+
+# for tag in tag_list:
+
+#     CTbed_path = sorted(glob.glob(f"{CTAC_bed_folder}*_{tag[1:]}_*.nii"))[0]
+#     CTbed_file = nib.load(CTbed_path)
+#     CTbed_data = CTbed_file.get_fdata()
+#     # print(f"For tag {tag}, CT bed path: {CTbed_path}")
+#     # print()
+
+#     DLCTAC_path = f"{DLCTAC_folder}E4{tag[2:]}_CTAC_DL_oriCTAC.nii.gz"
+#     DLCTAC_file = nib.load(DLCTAC_path)
+#     DLCTAC_data = DLCTAC_file.get_fdata()[:-1, :-1, :]
+
+#     # renormalize the DLCTAC data
+#     DLCTAC_data = (DLCTAC_data - MIN_CT) / WRONG_RANGE_CT
+#     DLCTAC_data = DLCTAC_data * RANGE_CT + MIN_CT
+
+#     # print(f"{tag}: ORIGINAL {CTbed_data.shape}, DLCTAC {DLCTAC_data.shape}")
+
+#     # print("<" * 50)
+#     DL_mask = DLCTAC_data > -500
+#     for z in range(DLCTAC_data.shape[2]):
+#         DL_mask[:, :, z] = binary_fill_holes(DL_mask[:, :, z])
+#     # iterate through the z axis for filling holes
+
+#     # replace the CT bed with DLCTAC using the mask
+#     CTbed_data[DL_mask] = DLCTAC_data[DL_mask]
+
+#     # save the data
+#     save_path = os.path.join(save_folder, f"E4{tag[2:]}_CTAC_DL_bed_fillholes.nii.gz")
+#     save_nii = nib.Nifti1Image(CTbed_data, CTbed_file.affine, CTbed_file.header)
+#     nib.save(save_nii, save_path)
+#     print(f"Data saved at {save_path}")
+
+# E4055: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4058: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4061: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4066: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4068: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4069: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4073: ORIGINAL (512, 512, 515), DLCTAC (513, 513, 515)
+# E4074: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4077: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4078: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4079: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4081: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4084: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4091: ORIGINAL (512, 512, 515), DLCTAC (513, 513, 515)
+# E4092: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4094: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4096: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4098: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4099: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4103: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4105: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4106: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4114: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4115: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4118: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4120: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4124: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4125: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4128: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4129: ORIGINAL (512, 512, 551), DLCTAC (513, 513, 551)
+# E4130: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4131: ORIGINAL (512, 512, 299), DLCTAC (513, 513, 299)
+# E4134: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4137: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4138: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+# E4139: ORIGINAL (512, 512, 335), DLCTAC (513, 513, 335)
+
+
+for tag in tag_list:
+
+    src_TOFNAC_path = f"{TOFNAC_data_folder}PET_TOFNAC_{tag}.nii.gz"
+    src_CTAC_path = sorted(glob.glob(f"{CTAC_bed_folder}*_{tag[1:]}_*.nii"))[0]
+    src_DLCT_path = f"{DLCTAC_bed_fillholes_folder}E4{tag[2:]}_CTAC_DL_bed_fillholes.nii.gz"
+
+    dst_TOFNAC_path = f"{save_folder_TOFNAC}TOFNAC_{tag}.nii.gz"
+    dst_CTAC_path = f"{save_folder_CTAC}CTAC_{tag}.nii.gz"
+    dst_DLCT_path = f"{save_folder_DLCT}DLCT_{tag}.nii.gz"
+
+    cmd_TOFNAC = f"cp {src_TOFNAC_path} {dst_TOFNAC_path}"
+    cmd_CTAC = f"cp {src_CTAC_path} {dst_CTAC_path}"
+    cmd_DLCT = f"cp {src_DLCT_path} {dst_DLCT_path}"
+
+    os.system(cmd_TOFNAC)
+    os.system(cmd_CTAC)
+    os.system(cmd_DLCT)
+
+    print(f"{tag}: {src_TOFNAC_path} -> {dst_TOFNAC_path}")
+    print(f"{tag}: {src_CTAC_path} -> {dst_CTAC_path}")
+    print(f"{tag}: {src_DLCT_path} -> {dst_DLCT_path}")
+
+    print()
