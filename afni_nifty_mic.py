6d2bb7ba1793c525defb50b01302969b716302ce
WeijieChen2017 <weijiechen1994@gmail.com>
2024-10-02 16:35:22 -0500

    Good morning!

---
+++ afni_nifty_mic.py
@@ -0,0 +1,29 @@
+# niftymic_reconstruct_volume \
+#   --filenames HNJ120_axial_z140.nii.gz HNJ120_coronal_z140.nii.gz HNJ120_sagittal_z140.nii.gz \
+#   --filenames-masks HNJ120_axial_z140_mask.nii.gz HNJ120_coronal_z140_mask.nii.gz HNJ120_sagittal_z140_mask.nii.gz \
+#   --output HNJ120_axial_z140_niftyMIC.nii.gz
+
+# niftymic_reconstruct_volume \
+#   --filenames HNJ120_axial_z280.nii.gz HNJ120_coronal_z280.nii.gz HNJ120_sagittal_z280.nii.gz \
+#   --filenames-masks HNJ120_axial_z280_mask.nii.gz HNJ120_coronal_z280_mask.nii.gz HNJ120_sagittal_z280_mask.nii.gz \
+#   --output HNJ120_axial_z280_niftyMIC.nii.gz
+
+# niftymic_reconstruct_volume \
+#   --filenames HNJ120_axial_z420.nii.gz HNJ120_coronal_z420.nii.gz HNJ120_sagittal_z420.nii.gz \
+#   --filenames-masks HNJ120_axial_z420_mask.nii.gz HNJ120_coronal_z420_mask.nii.gz HNJ120_sagittal_z420_mask.nii.gz \
+#   --output HNJ120_axial_z420_niftyMIC.nii.gz
+
+z_list = ["140", "280", "420"]
+direction_list = ["axial", "coronal", "sagittal"]
+
+import os
+
+for z in z_list:
+    for direction in direction_list:
+        filenames = []
+        filenames_masks = []
+        for i in range(3):
+            filenames.append(f"HNJ120_{direction}_z{z}_offset1024..nii.gz")
+            filenames_masks.append(f"HNJ120_{direction}_z{z}_mask.nii.gz")
+        output = f"HNJ120_{direction}_z{z}_niftyMIC_offset1024.nii.gz"
+        print(f"niftymic_reconstruct_volume --filenames {' '.join(filenames)} --filenames-masks {' '.join(filenames_masks)} --output {output}")
