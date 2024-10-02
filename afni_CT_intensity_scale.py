705a94301a55fe5946a56444a0d9c509e2d0fc16
WeijieChen2017 <weijiechen1994@gmail.com>
2024-10-02 16:20:18 -0500

    Good morning!

---
+++ afni_CT_intensity_scale.py
@@ -0,0 +1,27 @@
+folder = "results/cv1_256/test_mask/zcut3"
+file_list = [
+    "HNJ120_axial_z140.nii.gz",
+    "HNJ120_axial_z280.nii.gz",
+    "HNJ120_axial_z420.nii.gz",
+    "HNJ120_coronal_z140.nii.gz",
+    "HNJ120_coronal_z280.nii.gz",
+    "HNJ120_coronal_z420.nii.gz",
+    "HNJ120_sagittal_z140.nii.gz",
+    "HNJ120_sagittal_z280.nii.gz",
+    "HNJ120_sagittal_z420.nii.gz",
+]
+
+# we load every file, add 1024 to the intensity values, and save the result as name_offset1024.nii.gz
+
+import nibabel as nib
+
+for file in file_list:
+    file_path = os.path.join(folder, file)
+    nii_file = nib.load(file_path)
+    nii_data = nii_file.get_fdata()
+    nii_data += 1024
+    new_save_name = file.split(".")[0] + "_offset1024.nii.gz"
+    new_save_path = os.path.join(folder, new_save_name)
+    new_nii = nib.Nifti1Image(nii_data, nii_file.affine)
+    nib.save(new_nii, new_save_path)
+    print(f"Saved {new_save_path}")
