3f336c9a719ef35ceb7d7893271b9a155ad18578
WeijieChen2017 <weijiechen1994@gmail.com>
2024-10-03 15:36:13 -0500

    Good morning!

--- afni_CT_intensity_scale.py
+++ afni_CT_intensity_scale.py
@@ -39,7 +39,7 @@ other_case = ["KWX131", "LBO118", "NAF069", "NIR103", "RSE114"]
 # 3dZcutup -prefix HNJ120_sagittal_z280_mask.nii.gz -keep 140 279 HNJ120_CTAC_pred_sagittal_mask_cv1.nii.gz
 # 3dZcutup -prefix HNJ120_sagittal_z420_mask.nii.gz -keep 280 419 HNJ120_CTAC_pred_sagittal_mask_cv1.nii.gz
 
-3dZcutup_commands = [
+commands_3dZcutup = [
     "3dZcutup -prefix HNJ120_axial_z140.nii.gz -keep 0 139 HNJ120_CTAC_pred_axial_rescale_cv1.nii.gz",
     "3dZcutup -prefix HNJ120_axial_z280.nii.gz -keep 140 279 HNJ120_CTAC_pred_axial_rescale_cv1.nii.gz",
     "3dZcutup -prefix HNJ120_axial_z420.nii.gz -keep 280 419 HNJ120_CTAC_pred_axial_rescale_cv1.nii.gz",
@@ -62,7 +62,7 @@ other_case = ["KWX131", "LBO118", "NAF069", "NIR103", "RSE114"]
 
 for other_case_name in other_case:
     print()
-    for command in 3dZcutup_commands:
+    for command in commands_3dZcutup:
         new_command = command.replace("HNJ120", other_case_name)
         print(new_command)
     print()
