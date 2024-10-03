5fabcad821a334541ef946af6d78058c21698d0e
WeijieChen2017 <weijiechen1994@gmail.com>
2024-10-03 16:01:01 -0500

    Good morning!

---
+++ afni_cmd_1.sh
@@ -0,0 +1,9 @@
+niftymic_reconstruct_volume --filenames KWX131_axial_z140_offset1024.nii.gz KWX131_coronal_z140_offset1024.nii.gz KWX131_sagittal_z140_offset1024.nii.gz --filenames-masks KWX131_axial_z140_mask.nii.gz KWX131_coronal_z140_mask.nii.gz KWX131_sagittal_z140_mask.nii.gz --output KWX131_sagittal_z140_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames KWX131_axial_z280_offset1024.nii.gz KWX131_coronal_z280_offset1024.nii.gz KWX131_sagittal_z280_offset1024.nii.gz --filenames-masks KWX131_axial_z280_mask.nii.gz KWX131_coronal_z280_mask.nii.gz KWX131_sagittal_z280_mask.nii.gz --output KWX131_sagittal_z280_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames KWX131_axial_z420_offset1024.nii.gz KWX131_coronal_z420_offset1024.nii.gz KWX131_sagittal_z420_offset1024.nii.gz --filenames-masks KWX131_axial_z420_mask.nii.gz KWX131_coronal_z420_mask.nii.gz KWX131_sagittal_z420_mask.nii.gz --output KWX131_sagittal_z420_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames LBO118_axial_z140_offset1024.nii.gz LBO118_coronal_z140_offset1024.nii.gz LBO118_sagittal_z140_offset1024.nii.gz --filenames-masks LBO118_axial_z140_mask.nii.gz LBO118_coronal_z140_mask.nii.gz LBO118_sagittal_z140_mask.nii.gz --output LBO118_sagittal_z140_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames LBO118_axial_z280_offset1024.nii.gz LBO118_coronal_z280_offset1024.nii.gz LBO118_sagittal_z280_offset1024.nii.gz --filenames-masks LBO118_axial_z280_mask.nii.gz LBO118_coronal_z280_mask.nii.gz LBO118_sagittal_z280_mask.nii.gz --output LBO118_sagittal_z280_niftyMIC_offset1024.nii.gz
---
+++ afni_cmd_3.sh
@@ -0,0 +1,9 @@
+niftymic_reconstruct_volume --filenames NIR103_axial_z280_offset1024.nii.gz NIR103_coronal_z280_offset1024.nii.gz NIR103_sagittal_z280_offset1024.nii.gz --filenames-masks NIR103_axial_z280_mask.nii.gz NIR103_coronal_z280_mask.nii.gz NIR103_sagittal_z280_mask.nii.gz --output NIR103_sagittal_z280_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames NIR103_axial_z420_offset1024.nii.gz NIR103_coronal_z420_offset1024.nii.gz NIR103_sagittal_z420_offset1024.nii.gz --filenames-masks NIR103_axial_z420_mask.nii.gz NIR103_coronal_z420_mask.nii.gz NIR103_sagittal_z420_mask.nii.gz --output NIR103_sagittal_z420_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames RSE114_axial_z140_offset1024.nii.gz RSE114_coronal_z140_offset1024.nii.gz RSE114_sagittal_z140_offset1024.nii.gz --filenames-masks RSE114_axial_z140_mask.nii.gz RSE114_coronal_z140_mask.nii.gz RSE114_sagittal_z140_mask.nii.gz --output RSE114_sagittal_z140_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames RSE114_axial_z280_offset1024.nii.gz RSE114_coronal_z280_offset1024.nii.gz RSE114_sagittal_z280_offset1024.nii.gz --filenames-masks RSE114_axial_z280_mask.nii.gz RSE114_coronal_z280_mask.nii.gz RSE114_sagittal_z280_mask.nii.gz --output RSE114_sagittal_z280_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames RSE114_axial_z420_offset1024.nii.gz RSE114_coronal_z420_offset1024.nii.gz RSE114_sagittal_z420_offset1024.nii.gz --filenames-masks RSE114_axial_z420_mask.nii.gz RSE114_coronal_z420_mask.nii.gz RSE114_sagittal_z420_mask.nii.gz --output RSE114_sagittal_z420_niftyMIC_offset1024.nii.gz
