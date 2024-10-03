1e5d74d3dc2a3c2ae1a4da41029cca52f4702cbd
WeijieChen2017 <weijiechen1994@gmail.com>
2024-10-03 16:01:01 -0500

    Good morning!

---
+++ afni_cmd_2.sh
@@ -0,0 +1,9 @@
+niftymic_reconstruct_volume --filenames LBO118_axial_z420_offset1024.nii.gz LBO118_coronal_z420_offset1024.nii.gz LBO118_sagittal_z420_offset1024.nii.gz --filenames-masks LBO118_axial_z420_mask.nii.gz LBO118_coronal_z420_mask.nii.gz LBO118_sagittal_z420_mask.nii.gz --output LBO118_sagittal_z420_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames NAF069_axial_z140_offset1024.nii.gz NAF069_coronal_z140_offset1024.nii.gz NAF069_sagittal_z140_offset1024.nii.gz --filenames-masks NAF069_axial_z140_mask.nii.gz NAF069_coronal_z140_mask.nii.gz NAF069_sagittal_z140_mask.nii.gz --output NAF069_sagittal_z140_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames NAF069_axial_z280_offset1024.nii.gz NAF069_coronal_z280_offset1024.nii.gz NAF069_sagittal_z280_offset1024.nii.gz --filenames-masks NAF069_axial_z280_mask.nii.gz NAF069_coronal_z280_mask.nii.gz NAF069_sagittal_z280_mask.nii.gz --output NAF069_sagittal_z280_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames NAF069_axial_z420_offset1024.nii.gz NAF069_coronal_z420_offset1024.nii.gz NAF069_sagittal_z420_offset1024.nii.gz --filenames-masks NAF069_axial_z420_mask.nii.gz NAF069_coronal_z420_mask.nii.gz NAF069_sagittal_z420_mask.nii.gz --output NAF069_sagittal_z420_niftyMIC_offset1024.nii.gz
+
+niftymic_reconstruct_volume --filenames NIR103_axial_z140_offset1024.nii.gz NIR103_coronal_z140_offset1024.nii.gz NIR103_sagittal_z140_offset1024.nii.gz --filenames-masks NIR103_axial_z140_mask.nii.gz NIR103_coronal_z140_mask.nii.gz NIR103_sagittal_z140_mask.nii.gz --output NIR103_sagittal_z140_niftyMIC_offset1024.nii.gz
