44326566ce340ea88650757a4edab248dbb4d716
WeijieChen2017 <weijiechen1994@gmail.com>
2024-10-03 15:27:09 -0500

    Good morning!

--- afni_CT_intensity_scale.py
+++ afni_CT_intensity_scale.py
@@ -11,18 +11,23 @@ file_list = [
     "HNJ120_sagittal_z420.nii.gz",
 ]
 
+other_case = ["KWX131", "LBO118", "NAF069", "NIR103", "RSE114"]
+
 # we load every file, add 1024 to the intensity values, and save the result as name_offset1024.nii.gz
 
 import os
 import nibabel as nib
 
-for file in file_list:
-    file_path = os.path.join(folder, file)
-    nii_file = nib.load(file_path)
-    nii_data = nii_file.get_fdata()
-    nii_data += 1024
-    new_save_name = file.split(".")[0] + "_offset1024.nii.gz"
-    new_save_path = os.path.join(folder, new_save_name)
-    new_nii = nib.Nifti1Image(nii_data, nii_file.affine)
-    nib.save(new_nii, new_save_path)
-    print(f"Saved {new_save_path}")
+for other_case_name in other_case:
+
+    for old_file in file_list:
+        file_path = old_file.replace("HNJ120", other_case_name)
+        file_path = os.path.join(folder, file)
+        nii_file = nib.load(file_path)
+        nii_data = nii_file.get_fdata()
+        nii_data += 1024
+        new_save_name = file.split(".")[0] + "_offset1024.nii.gz"
+        new_save_path = os.path.join(folder, new_save_name)
+        new_nii = nib.Nifti1Image(nii_data, nii_file.affine)
+        nib.save(new_nii, new_save_path)
+        print(f"Saved {new_save_path}")
