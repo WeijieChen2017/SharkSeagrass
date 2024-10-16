5f9bebab4322462f54d5247f09eeeab25eaec741
WeijieChen2017 <weijiechen1994@gmail.com>
2024-10-16 02:36:44 -0500

    Good morning!

--- ISBI2025_ldm_iceEnc.py
+++ ISBI2025_ldm_iceEnc.py
@@ -1,7 +1,7 @@
 # cv_list = ["cv1", "cv3", "cv4"] # for ldm
 cv_list = ["cv0", "cv1"] # for iceEnc
 
-split_list = ["train", "val", "test"]
+split_list = ["test", "train", "val"]
 
 data_fusion_list = ["axial", "sagittal", "coronal", "average", "median"]
 
