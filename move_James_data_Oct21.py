b9128c5d97e5686e2c92cbf297774d5ba092f9a4
WinstonChen2017 <weijiechen1994@gmail.com>
2024-10-21 20:41:27 -0500

    start for v3 training

--- move_James_data_Oct21.py
+++ move_James_data_Oct21.py
@@ -1,8 +1,8 @@
 import os
 import glob
 
-TOFNAC_dir = "/James_data_v3/Duetto_Output_B100_nii/"
-CTACIVV_dir = "/James_data_v3/part3/"
+TOFNAC_dir = "James_data_v3/Duetto_Output_B100_nii/"
+CTACIVV_dir = "James_data_v3/part3/"
 
 TOFNAC_dir_list =sorted(glob.glob(TOFNAC_dir + "*/*.nii"))
 for TOFNAC_path in TOFNAC_dir_list:
