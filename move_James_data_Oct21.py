e9e37ef7dd73ea87cd9bddf2e29f4ba10cf6022d
WinstonChen2017 <weijiechen1994@gmail.com>
2024-10-21 20:49:09 -0500

    start for v3 training

--- move_James_data_Oct21.py
+++ move_James_data_Oct21.py
@@ -12,9 +12,9 @@ for TOFNAC_path in TOFNAC_dir_list:
     case_name = case_name.split(" ")[-2]
     CTACIVV_path = f"{CTACIVV_dir}CTACIVV_{case_name[1:]}.nii"
     print(case_name)
-    move_cmd_TOFNAC = f"mv {TOFNAC_path} James_data_v3/TOFNAC_{case_name}.nii"
-    move_cmd_CTACIVV = f"mv {CTACIVV_path} James_data_v3/CTACIVV_{case_name}.nii"
+    move_cmd_TOFNAC = f"mv \"{TOFNAC_path}\" James_data_v3/TOFNAC_{case_name}.nii"
+    # move_cmd_CTACIVV = f"mv {CTACIVV_path} James_data_v3/CTACIVV_{case_name}.nii"
     print(move_cmd_TOFNAC)
-    print(move_cmd_CTACIVV)
+    # print(move_cmd_CTACIVV)
     os.system(move_cmd_TOFNAC)
-    os.system(move_cmd_CTACIVV)
+    # os.system(move_cmd_CTACIVV)
