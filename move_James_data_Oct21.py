c7a747ad1647e036800c8ca25b4fc2ce71b71106
WinstonChen2017 <weijiechen1994@gmail.com>
2024-10-21 20:46:28 -0500

    start for v3 training

--- move_James_data_Oct21.py
+++ move_James_data_Oct21.py
@@ -6,10 +6,11 @@ CTACIVV_dir = "James_data_v3/part3/"
 
 TOFNAC_dir_list =sorted(glob.glob(TOFNAC_dir + "*/*.nii"))
 for TOFNAC_path in TOFNAC_dir_list:
+    print()
     case_name = TOFNAC_path.split("/")[-2].split(".")[0]
     # PET TOFNAC E4237 B100
     case_name = case_name.split(" ")[-2]
-    CTACIVV_path = f"{CTACIVV_dir}CTACIVV_{case_name[1:]}.nii.gz"
+    CTACIVV_path = f"{CTACIVV_dir}CTACIVV_{case_name[1:]}.nii"
     print(case_name)
     copy_cmd_TOFNAC = f"cp {TOFNAC_path} James_data_v3/TOFNAC_{case_name}.nii"
     copy_cmd_CTACIVV = f"cp {CTACIVV_path} James_data_v3/CTACIVV_{case_name}.nii"
