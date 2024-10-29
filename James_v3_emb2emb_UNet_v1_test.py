570a2b652655f2f1801437ab4670b07648b15072
WinstonChen2017 <weijiechen1994@gmail.com>
2024-10-29 03:30:11 -0500

    start training UNet in three directions

--- James_v3_emb2emb_UNet_v1_test.py
+++ James_v3_emb2emb_UNet_v1_test.py
@@ -213,21 +213,21 @@ for case_name in test_list:
         np.save(axial_no_VQ_path, axial_no_VQ)
         print(f"axial_no_VQ saved to {axial_no_VQ_path}, shape: {axial_no_VQ.shape}")
     
-    if os.path.exists(axial_VQ_order_one_path):
-        axial_VQ_order_one = np.load(axial_VQ_order_one_path)
-        print(f"axial_VQ_order_one loaded from {axial_VQ_order_one_path}, shape: {axial_VQ_order_one.shape}")
-    else:
-        axial_VQ_order_one = VQ_NN_embedings(vq_weights, axial_no_VQ, dist_order=1)
-        np.save(axial_VQ_order_one_path, axial_VQ_order_one)
-        print(f"axial_VQ_order_one saved to {axial_VQ_order_one_path}")
+    # if os.path.exists(axial_VQ_order_one_path):
+    #     axial_VQ_order_one = np.load(axial_VQ_order_one_path)
+    #     print(f"axial_VQ_order_one loaded from {axial_VQ_order_one_path}, shape: {axial_VQ_order_one.shape}")
+    # else:
+    #     axial_VQ_order_one = VQ_NN_embedings(vq_weights, axial_no_VQ, dist_order=1)
+    #     np.save(axial_VQ_order_one_path, axial_VQ_order_one)
+    #     print(f"axial_VQ_order_one saved to {axial_VQ_order_one_path}")
 
-    if os.path.exists(axial_VQ_order_two_path):
-        axial_VQ_order_two = np.load(axial_VQ_order_two_path)
-        print(f"axial_VQ_order_two loaded from {axial_VQ_order_two_path}, shape: {axial_VQ_order_two.shape}")
-    else:
-        axial_VQ_order_two = VQ_NN_embedings(vq_weights, axial_no_VQ, dist_order=2)
-        np.save(axial_VQ_order_two_path, axial_VQ_order_two)
-        print(f"axial_VQ_order_two saved to {axial_VQ_order_two_path}")
+    # if os.path.exists(axial_VQ_order_two_path):
+    #     axial_VQ_order_two = np.load(axial_VQ_order_two_path)
+    #     print(f"axial_VQ_order_two loaded from {axial_VQ_order_two_path}, shape: {axial_VQ_order_two.shape}")
+    # else:
+    #     axial_VQ_order_two = VQ_NN_embedings(vq_weights, axial_no_VQ, dist_order=2)
+    #     np.save(axial_VQ_order_two_path, axial_VQ_order_two)
+    #     print(f"axial_VQ_order_two saved to {axial_VQ_order_two_path}")
 
     len_z = axial_no_VQ.shape[0]
     recon_axial_no_VQ = np.zeros((gt_x, gt_y, len_z), dtype=np.float32)
