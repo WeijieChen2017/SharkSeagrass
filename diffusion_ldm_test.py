8286fb1e617b9510f259c778953a3c1ad7d4f33e
WinstonChen2017 <weijiechen1994@gmail.com>
2024-11-21 22:27:39 -0600

    load the trained model

--- diffusion_ldm_test.py
+++ diffusion_ldm_test.py
@@ -337,6 +337,7 @@ with torch.no_grad():
             # encode masked image and concat downsampled mask
             # c = model.cond_stage_model.encode(batch["image"]) # channel = 3
             c = model.first_stage_model.encode(batch["image"])
+            np.save()
             # cc = torch.nn.functional.interpolate(batch["mask"],
             #                                         size=c.shape[-2:]) # channel = 1
             # c = torch.cat((c, cc), dim=1) # channel = 4
